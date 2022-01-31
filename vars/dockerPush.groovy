e141f3a734a619aeb47f3a175896f4505a35dae1
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-01-31 17:24:54 +0000

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -5,7 +5,7 @@ def call() {
 
                     container('docker') {
                         echo "Running Docker Push"
-                        sh 'docker login -u ${setting.token_registry} -p ${setting.token_registry} https://registry.digitalocean.com'
+                        sh 'docker login -u setting.token_registry -p setting.token_registry https://registry.digitalocean.com'
                         dockerPush ( registry_url: setting.url_images_registry, image_name: config.service_name)
                     }
 }
