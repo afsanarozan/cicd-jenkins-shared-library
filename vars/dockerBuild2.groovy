4c3176aaf9b363f70a1c4272e6a379c322f15a2c
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-01-31 17:07:21 +0000

    Update dockerBuild2.groovy

--- vars/dockerBuild2.groovy
+++ vars/dockerBuild2.groovy
@@ -7,9 +7,10 @@ def call() {
                         echo "Running Docker Build"
                         dockerBuild(registry_url: setting.url_images_registry, image_name: config.service_name)
                     }
+}
 
 def dockerBuild(Map args) {
   sh "docker build -t ${args.registry_url}/${args.image_name}:beta ."
-  }      
-}
+}      
+
 
