78c7d504cbd1f200e55d2d69db2349c920115d22
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-01-31 16:27:39 +0000

    Update dockerBuild2.groovy

--- vars/dockerBuild2.groovy
+++ vars/dockerBuild2.groovy
@@ -5,7 +5,6 @@ def call() {
 
                     container('Docker') {
                         sh 'docker images'
-                        sh 'docker login -u setting.token_registry -p setting.token_registry https://registry.digitalocean.com'
                         docker.build("setting.url_images_registry/config.service_name:beta")
                     }
 }
