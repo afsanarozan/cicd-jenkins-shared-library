c86766f29c2ef9d8bccb12ac568561491d87b5ee
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-01-31 17:16:50 +0000

    Add new file

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,17 @@
+def call() {
+  def config = pipelineCfg()
+  def envar = checkoutTagging()
+  def setting = settings()
+
+                    container('docker') {
+                        echo "Running Docker Push"
+                        sh 'docker login -u ${setting.token_registry} -p ${setting.token_registry} https://registry.digitalocean.com'
+                        dockerPush ( registry_url: setting.url_images_registry, image_name: config.service_name)
+                    }
+}
+
+def dockerPush(Map args) {
+    sh 'docker push ${args.registry_url}/${args.image_name}:beta'
+    sh 'docker rmi ${args.registry_url}/${args.image_name}:beta'
+}
+                        
