0f37ad77abd2c54b6f94a15373d9db085cc65eb2
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-01-31 19:01:28 +0000

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -6,7 +6,7 @@ def call() {
                     container('docker') {
                         echo "Running Docker Push"
                         echo "${setting.url_images_registry}"
-                        docker.withRegistry("${setting.url_images_registry}", token_registry) {
+                        docker.withRegistry("https://${setting.url_images_registry}", setting.token) {
                         echo "${setting.url_images_registry}"
                         dockerPush(registry_url: setting.url_images_registry, image_name: config.service_name, token: setting.token_registry)
                     }
