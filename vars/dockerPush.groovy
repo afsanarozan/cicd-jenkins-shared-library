3004f6c5f6e4ceb43bf832d691d269ca3bb87b89
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-03 11:28:55 +0700

    edit dockerpush groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -6,10 +6,8 @@ def call(Map envar) {
                     container('docker') {
                         echo "Running Docker Push"
                         echo "${envar.container_registry}"
-                        docker.withRegistry("https://${envar.container_registry}", token_registry) {
-                        echo "${setting.container_registry}"
+                        sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
                         dockerPush(registry_url: envar.container_registry, image_name: envar.image_name, token: envar.token_registry)
-                    }
                 }        
 }
 
