4239ba66f5886d4e168a80910a2be3072e834b3d
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-03 15:19:34 +0700

    edit groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -3,13 +3,14 @@ def call() {
 //   def setting = settings()
     echo "Deploy to kubernetes "
     container('base'){
-        withKubeConfig([credentialsId: 'nonprod-cluster'])
-        script {
+        withKubeConfig([credentialsId: 'nonprod-cluster']) {
+            script {
             sh """
                 kubectl config use-context do-sgp1-labs-nonproduction  
                 kubectl config get-contexts
                 kubectl get ns
             """
+            }
         }
     }        
     
