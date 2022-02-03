6fef41c6e0bdef91a14db28479f78574abab8dd4
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-03 13:45:44 +0700

    edit groovy

--- vars/helmPackage.groovy
+++ vars/helmPackage.groovy
@@ -4,5 +4,18 @@ def call(Map envar) {
     dir('helm') {
         checkout([$class: 'GitSCM', branches: [[name: "master"]], userRemoteConfigs: [[credentialsId: "6c8b6848-ca10-4862-b1a9-fe3e6d46da61", url: "${envar.helm_git}"]]])
         sh "ls"
+        helmLint(service_name)
+        helmPackage(service_name)
     }
+    sh "ls"
+}
+
+def helmLint(String service_name) {
+    echo "Running helm lint"
+    sh "helm lint ${service_name}"
+}
+
+def helmPackage(String service_name) {
+    echo "Running Helm Package"
+    sh "helm package ${service_name}"
 }
