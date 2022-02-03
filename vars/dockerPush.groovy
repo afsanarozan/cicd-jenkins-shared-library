cd4bf7718e413a10eda5ee2c8da747e380b9ec6e
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-03 15:48:00 +0700

    edit groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -3,12 +3,12 @@ def call(Map envar) {
 //   def envar = checkoutTagging()
 //  def setting = settings()
 
-                    container('docker') {
-                        echo "Running Docker Push"
-                        echo "${envar.container_registry}"
-                        sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
-                        dockerPush(registry_url: envar.container_registry, image_name: envar.image_name)
-                }        
+    container('docker') {
+        echo "Running Docker Push"
+        echo "${envar.container_registry}"
+        sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
+        dockerPush(registry_url: envar.container_registry, image_name: envar.image_name)
+    }        
 }
 
 def dockerPush(Map args) {
--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -3,15 +3,16 @@ def call() {
 //   def setting = settings()
     echo "Deploy to kubernetes "
     container('base'){
-        withKubeConfig([credentialsId: 'nonprod-cluster']) {
-            script {
-            sh """
-                kubectl config use-context do-sgp1-labs-nonproduction  
-                kubectl config get-contexts
-                kubectl get ns
-            """
+            dir('helm') {
+                withKubeConfig([credentialsId: 'nonprod-cluster']) {
+                script {
+                sh """
+                    kubectl config use-context do-sgp1-labs-nonproduction  
+                    kubectl config get-contexts
+                    helm
+                """
+                }
             }
         }
-    }        
-    
+    }       
 }
