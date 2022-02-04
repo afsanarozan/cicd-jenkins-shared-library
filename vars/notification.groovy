e635f0a4370cc8ec9fec2b523757794eca879b2b
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-04 10:40:43 +0700

    add notification stage

---
+++ vars/notification.groovy
@@ -0,0 +1,18 @@
+def call(Map var) {
+    echo "Job Success"
+    notifications(telegram_url: var.telegram_url, telegram_chatid: var.telegram_chatid, job: var.job, job_numb: var.job_numb, job_url: var.job_url)
+    echo "${env.BUILD_URL}"
+}
+
+
+def notifications(Map args) {
+    def message = "CICD Pipeline ${args.job} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time :"
+    parallel(
+        "Telegram": {
+            sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
+        },
+        "Jira": {
+            //jiraSend color: "${args.jira_url}", message: "${message}", channel: "${args.slack_channel}"
+        }
+    )
+}
