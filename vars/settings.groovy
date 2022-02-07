8cbff9a0fa225b92bf6581ed37bc013faed40783
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-07 03:05:32 +0000

    Update settings.groovy

--- vars/settings.groovy
+++ vars/settings.groovy
@@ -6,21 +6,6 @@ def call() {
   setting.accesstoken                       = "iUENiKyV6B5hPdoCxV3z"
   setting.tokenapi                          = "glpat-xa3dRUG8-dSg5rY4nNe5"
 
-  //private library configuration   
-  setting.go_private_lib                    = "https://gitlab.com/dslt/codesmells.git"
-  setting.go_private_lib_auth               = ""
-
-  // jfrog cli
-  setting.jfrog_cli                         = "https://getcli.jfrog.io"
-  setting.jfrog_user                        = "admin"
-
-  //tools configuration
-  setting.artifactory_url                   = "https://klikdevsecops.jfrog.io"
-  setting.container_registry_url            = "https://klikdevsecops.jfrog.io/klik-docker"
-  setting.registry_credential               = "jfrog-credential"
-  setting.sonarqube_enterprise_url          = "https://sq.toolchain.klik.digital"
-  setting.sonarqube_developer_url           = "https://sqd.toolchain.klik.digital"
-
   setting.url_images_registry               = "registry.digitalocean.com/labs-registry"
   setting.token_registry                    = "43366f93440e11863eadc59d62baf4f481a926e6a433c05ebdebc91368fecb13"
 
