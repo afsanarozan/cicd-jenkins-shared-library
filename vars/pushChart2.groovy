b7115ab7800dbddf54828aab69a43282ffabca2f
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-07 12:53:29 +0000

    Add new file

---
+++ vars/pushChart2.groovy
@@ -0,0 +1,21 @@
+def call(Map envar) {
+    def config = pipelineCfg()
+    echo "Running Helm Push"
+    
+    dir('Charts') {
+        sh "ls"
+            container('aws-cli'){
+            sh "aws s3 cp cms-api-0.1.0.tgz --endpoint-url https://labs-storage.sgp1.digitaloceanspaces.com s3://helm-charts/cms-api-0.1.0.tgz"
+        }
+    }
+}
+
+def helmLint(Map args) {
+    echo "Running helm lint"
+    sh "helm lint ${args.service_name}"
+}
+
+def helmPackage(Map args) {
+    echo "Running Helm Package"
+    sh "helm package ${args.service_name}"
+}
