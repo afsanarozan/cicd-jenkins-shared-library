32ddc09a0df760c3ab0a3865231dc8d37f58f3cc
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-07 13:28:10 +0000

    Update PodTemplates.groovy

--- src/com/shared/utils/PodTemplates.groovy
+++ src/com/shared/utils/PodTemplates.groovy
@@ -6,9 +6,24 @@ public void generalTemplate(body) {
         containerTemplate(name: 'helm', image: 'docker.io/alpine/helm', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'curl', image: 'centos', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'k6', image: 'coralspec/k6', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'sonarscanner', image: 'sonarscannerbuildkite/sonarscanner:0.1.1', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'aws', image: 'amazon/aws-cli', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'kubectl', image: 'dtzar/helm-kubectl', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'golang', image: 'golang:1.15', command: 'cat', ttyEnabled: true),
+        containerTemplate(
+              name: 'aws-cli', 
+              image: 'mesosphere/aws-cli', 
+              envVars: [ envVar(key: 'AWS_ACCESS_KEY_ID', value: 'YFRP3PS4LIJEOZVRUMMK'),
+                         envVar(key: 'AWS_SECRET_ACCESS_KEY', value: '0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs'),
+                         envVar(key: 'AWS_DEFAULT_REGION', value: 'sgp1')],
+              command: 'cat', 
+              ttyEnabled: true),
+            containerTemplate(
+              name: 'jfrog-go', 
+              image: 'releases-docker.jfrog.io/jfrog/jfrog-cli-full-v2', 
+              envVars: [ envVar(key: 'JFROG_CLI_LOG_LEVEL', value: 'DEBUG')],
+              command: 'cat', 
+              ttyEnabled: true),
         containerTemplate(name: 'base', image: 'dgrlabs/base-runner:latest', command: 'cat', ttyEnabled: true)],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body.call()
