aa27ba92ac29fca423836d52c0c254f3880f3b79
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-07 05:01:17 +0000

    Add new file

---
+++ vars/sonarqubeScanner2.groovy
@@ -0,0 +1,29 @@
+def call() {
+  def config = pipelineCfg()
+  def envar = checkoutTagging()
+
+    echo "Running Code Review With SonarQube"
+        echo "${config.service_name}"
+        withSonarQubeEnv(credentialsId: "${config.credential_sonarqube}", installationName: 'sonarqube') {
+                sonarScanGo(name_space: config.name_space, service_name: config.service_name, sonarSrc: config.sonarSrc)
+        }
+         
+}
+
+def sonarScanGo(Map args){
+        sh " sonar-scanner -X \
+        -Dsonar.projectKey=${args.name_space}-${args.service_name} \
+        -Dsonar.sources=${args.sonarSrc} \
+        -Dsonar.qualitygate.wait=true \
+        -Dsonar.language=go \
+        -Dsonar.go.file.suffixes=.go \
+        -Dsonar.tests=. \
+        -Dsonar.test.inclusions=**/**_test.go \
+        -Dsonar.test.exclusions=**/vendor/** \
+        -Dsonar.sources.inclusions=**/**.go \
+        -Dsonar.exclusions=**/**.xml \
+        -Dsonar.go.exclusions=**/*_test.go,**/vendor/**,**/testdata \
+        -Dsonar.tests.reportPaths=report-tests.out \
+        -Dsonar.go.govet.reportPaths=report-vet.out \
+        -Dsonar.go.coverage.reportPaths=coverage.out"
+}
