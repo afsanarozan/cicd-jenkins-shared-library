7d237be4b7893c132216f53156562ee9c312c814
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-08 09:35:47 +0700

    add job status

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,14 +1,14 @@
 def call(Map var) {
     echo "Job Success"
     container('curl'){
-        notifications(telegram_url: var.telegram_url, telegram_chatid: var.telegram_chatid, job: var.job, job_numb: var.job_numb, job_url: var.job_url)
+        notifications(telegram_url: var.telegram_url, telegram_chatid: var.telegram_chatid, job: var.job, job_numb: var.job_numb, job_url: var.job_url, job_status: var.job_status)
         echo "${env.BUILD_URL}"
     }
 }
 
 
 def notifications(Map args) {
-    def message = "CICD Pipeline ${args.job} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
+    def message = "CICD Pipeline ${args.job} ${args.job_status} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
     parallel(
         "Telegram": {
             sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
