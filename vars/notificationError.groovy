9d90a78f98bcfba24c4ed8830e3b2f96d16551fc
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-08 09:27:27 +0700

    add notification error

--- vars/notificationError.groovy
+++ vars/notificationError.groovy
@@ -1,5 +1,5 @@
 def call(Map var) {
-    echo "Job Success"
+    echo "Job Failed"
     container('curl'){
         notifications(telegram_url: var.telegram_url, telegram_chatid: var.telegram_chatid, job: var.job, job_numb: var.job_numb, job_url: var.job_url)
         echo "${env.BUILD_URL}"
@@ -8,7 +8,7 @@ def call(Map var) {
 
 
 def notifications(Map args) {
-    def message = "CICD Pipeline ${args.job} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
+    def message = "CICD Pipeline ${args.job} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}" 
     parallel(
         "Telegram": {
             sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
