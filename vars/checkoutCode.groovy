31a606be02fd014cc4b78f51c8cb1136405e6938
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-10 02:38:45 +0000

    Add new file

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,46 @@
+def call() {
+  def config = pipelineCfg()
+  def envar = [:]
+  sh "printenv | sort"
+  
+  switch(env.BRANCH_NAME) {
+  
+    case 'staging':
+      if (env.gitlabActionType == 'NOTE' && env.gitlabTriggerPhrase == 'approved' ) {
+        envar.branch      = "staging"
+        envar.environment = 'staging'
+        envar.version     = "beta"
+      }
+      break;
+   case 'master': 
+     if (gitlabActionType == 'TAG_PUSH') {
+        envar.branch      = "master"
+        envar.environment = 'production'
+        envar.version     = "release"
+      }
+      break;
+    case 'dev':
+      if (gitlabActionType == 'PUSH') {
+        envar.branch      = "dev"
+        envar.environment = 'dev'
+        envar.version     = "alpha"
+      }
+      break;  
+  }
+
+  checkout_code(config, envar.branch)
+  
+  return envar
+  
+}
+def checkout_code (config, branch) {
+    echo "checkout branch ${branch}"
+    checkout changelog: true, poll: true, scm: [
+      $class: 'GitSCM',
+      branches: [[name: "${branch}"]],
+      doGenerateSubmoduleConfigurations: false,
+      submoduleCfg: [],
+      userRemoteConfigs: [[credentialsId: 'gitlab-auth-token', url: "${config.git_url}"]]
+    ]
+}
+
