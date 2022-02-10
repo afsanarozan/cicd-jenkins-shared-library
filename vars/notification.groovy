5e19eabd7ae674af76a0f5c6811e083e65a101d2
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-02-10 09:45:18 +0700

    edit notification

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -2,14 +2,14 @@ def call(Map var) {
     def config = pipelineCfg()
     echo "Job Success"
     container('curl'){
-        notifications(telegram_url: config.telegram_url, telegram_chatid: config.telegram_chatid, job: env.JOB_NAME, job_numb: env.JOB_NUMB, job_url: env.BUILD_URL, job_status: config.job_status)
+        notifications(telegram_url: config.telegram_url, telegram_chatid: config.telegram_chatid, job: env.JOB_NAME, job_numb: env.BUILD_NUMBER, job_url: env.BUILD_URL)
         echo "${env.BUILD_URL}"
     }
 }
 
 
 def notifications(Map args) {
-    def message = "CICD Pipeline ${args.job} ${args.job_status} with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
+    def message = "CICD Pipeline ${args.job} SUCCESS with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
     parallel(
         "Telegram": {
             sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
