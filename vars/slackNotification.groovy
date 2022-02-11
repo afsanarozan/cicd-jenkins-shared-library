25f5101cf81d15d68b5a2afc3830194e96d3b092
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-11 04:04:05 +0000

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,8 +1,10 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
+    def slackResponse = slackSend(channel: "jenkins")
     slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
   }
-  else if( buildResult == "FAILURE" ) { 
+  else if( buildResult == "FAILURE" ) {
+    def slackResponse = slackSend(channel: "jenkins")
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
   }
 }
