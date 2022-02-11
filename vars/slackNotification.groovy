c86a9c9ae999dc91099f64d2e2912ab29bede4f3
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-11 04:27:53 +0000

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,20 +1,31 @@
-def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    def slackResponse = slackSend(channel: "jenkins")
-    slackSend(channel: slackResponse.threadId)
-    slackSend(
-        channel: slackResponse.threadId,
-        replyBroadcast: true,
-    )
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-  }
-  else if( buildResult == "FAILURE" ) {
-    def slackResponse = slackSend(channel: "jenkins")
-    slackSend(channel: slackResponse.threadId)
-    slackSend(
-        channel: slackResponse.threadId,
-        replyBroadcast: true,
-    )
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
-  }
+def notifySlack(String buildStatus = 'STARTED') {
+    // Build status of null means success.
+    buildStatus = buildStatus ?: 'SUCCESS'
+
+    def color
+
+    if (buildStatus == 'STARTED') {
+        color = '#D4DADF'
+    } else if (buildStatus == 'SUCCESS') {
+        color = '#BDFFC3'
+    } else {
+        color = '#FF9FA1'
+    }
+
+    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+    slackSend(color: color, message: msg)
+}
+
+node {
+    try {
+        notifySlack()
+
+        // Existing build steps.
+    } catch (e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+        notifySlack(currentBuild.result)
+    }
 }
