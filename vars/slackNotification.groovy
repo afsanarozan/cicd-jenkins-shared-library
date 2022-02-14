576aca916072e3fe6e5e97559624d6521e882846
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-14 05:32:42 +0000

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -9,14 +9,14 @@ def doError = "0"
         sh """
         echo "Failure :("
         error "Test failed on purpose, doError == str(1)"
-        slackSend channel: '#jenkins',
+        def slackResponse = slackSend channel: '#jenkins',
                     color: COLOR_MAP[currentBuild.currentResult],
                         message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}"
         """
     }else if (doError == '0'){
         sh """
         echo "Succes :"
-        slackSend channel: '#jenkins',
+        def slackResponse = slackSend channel: '#jenkins',
                     color: COLOR_MAP[currentBuild.currentResult],
                         message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}"
         """
