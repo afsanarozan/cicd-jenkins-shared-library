50a98c9100084d3d6313eb3936459bf1b7037095
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-14 05:22:06 +0000

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -9,16 +9,24 @@ def doError = "0"
         sh """
         echo "Failure :("
         error "Test failed on purpose, doError == str(1)"
-        slackSend channel: "jenkins",
+        def slackResponse = slackSend(channel: "jenkins")
+        slackSend(channel: slackResponse.threadId)
+        slackSend(
+            channel: slackResponse.threadId,
             color: COLOR_MAP[currentBuild.currentResult],
-                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+            message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+        )
         """
     }else if (doError == '0'){
         sh """
         echo "Succes :"
-        slackSend channel: "jenkins",
+        def slackResponse = slackSend(channel: "jenkins")
+        slackSend(channel: slackResponse.threadId)
+        slackSend(
+            channel: slackResponse.threadId,
             color: COLOR_MAP[currentBuild.currentResult],
-                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+            message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+        )
         """
     }
 }
