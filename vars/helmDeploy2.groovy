c1472b3ed758bd42ae4d0eab9e118bfebfdbb033
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-14 01:14:02 +0000

    Update helmDeploy2.groovy

--- vars/helmDeploy2.groovy
+++ vars/helmDeploy2.groovy
@@ -29,13 +29,39 @@ switch(envar.version) {
 }
 
 container('base'){
-      withKubeConfig([credentialsId: DOcredential]) {
-              sh """
-                   kubectl config use-context ${context}
-                   kubectl get ns
-                   kubectl get pod -n ${namespace}
-               """        
-        }
-    }                       
+            dir('Charts') {
+                    withKubeConfig([credentialsId: 'nonprod-cluster']) {
+                    try {
+                        helmUpgrade(service_name: config.service_name, name_space: config.name_space)
+                    } catch (e) {
+                        helmInstall(service_name: config.service_name, name_space: config.name_space)
+                    }
+                }
+            }
+    }       
 }
+
+def helmUpgrade(Map args) {
+    sh """
+    cd ${args.service_name}
+    helm upgrade ${args.service_name} . -f values.yaml -n ${args.name_space}
+    """
+}
+
+def helmInstall(Map args) {
+    sh "cd ${args.service_name}"
+    sh "helm install ${args.service_name} . -f values.yaml -n ${args.name_space}"
+}
+
+
+//container('base'){
+//      withKubeConfig([credentialsId: DOcredential]) {
+//              sh """
+//                  kubectl config use-context ${context}
+//                   kubectl get ns
+//                   kubectl get pod -n ${namespace}
+//               """        
+//        }
+//    }                       
+//}
 
