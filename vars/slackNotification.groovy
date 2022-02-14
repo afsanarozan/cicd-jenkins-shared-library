5ee0cb92a1e38e8369e0b5a618d89fba4b4d763e
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-14 04:58:24 +0000

    Update slackNotification.groovy

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(String channel = '#jenkins'){
 def COLOR_MAP = [
     'SUCCESS': 'good', 
     'FAILURE': 'danger',
@@ -9,16 +9,16 @@ def doError = "0"
         sh """
         echo "Failure :("
         error "Test failed on purpose, doError == str(1)"
-        slackSend channel: '#jenkins',
+        slackSend channel(channel: channel,
             color: COLOR_MAP[currentBuild.currentResult],
-                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}")
         """
     }else if (doError == '0'){
         sh """
         echo "Succes :"
-        slackSend channel: '#jenkins',
+        slackSend channel(,
             color: COLOR_MAP[currentBuild.currentResult],
-                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}"
+                message: "*${currentBuild.currentResult}:* Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} \n More info at: ${env.BUILD_URL}")
         """
     }
 }
