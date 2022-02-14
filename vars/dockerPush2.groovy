984c66f1709f7ab490cbea4c9cab2b50ac945dd2
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-02-14 00:51:21 +0000

    Update dockerPush2.groovy

--- vars/dockerPush2.groovy
+++ vars/dockerPush2.groovy
@@ -5,7 +5,7 @@ def call() {
         container('docker') {
             echo "Running Docker Push"
             sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
-            dockerPush(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: ${envar.version}, dstVersion: "${envar.version}-${BUILD_NUMBER}")
+            dockerPush(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion: "${envar.version}-${BUILD_NUMBER}")
         }
     }else{
     skip()
