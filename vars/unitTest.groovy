8ecfeb66d92533d168836504adfdf577735ff470
afsana rozan naufal <afsanarozanaufal@gmail.com>
2022-02-16 06:01:44 +0000

    Update vars/unitTest.groovy, vars/notification.groovy

--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -41,9 +41,8 @@ def call() {
              }
 
              def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
-             def unitTest_score_controller = "Your score in directory controller is ${unitTestGetValue}"
-             echo "${unitTest_score_controller}"
-
+             def unitTest_score   = "Your score in directory controller is ${unitTestGetValue}"
+             echo "${unitTest_score}"
         }
 }
 
