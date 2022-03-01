cb95117b3cb5d779b2ceb09a3993e366a9797321
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-01 10:02:56 +0700

    add deploykubernetes

---
+++ vars/deployKubernetes.groovy
@@ -0,0 +1,24 @@
+def call(Map var) {
+    def config = pipelineCfg()
+    echo "Deploy to kubernetes "
+    container('base'){
+            withKubeConfig([credentialsId: 'nonprod-cluster']) {
+            try {
+                kubectlApply(service_name: config.service_name, name_space: config.name_space)
+            } catch (e) {
+                helmInstall(service_name: config.service_name, name_space: config.name_space)
+            }
+        }
+    }       
+}
+
+def kubectlApply(Map args) {
+    sh """
+    kubectl apply -f Deployment/ -n ${args.name_space}
+    """
+}
+
+def helmInstall(Map args) {
+    sh "cd ${args.service_name}"
+    sh "helm install ${args.service_name} . -f values.yaml -n ${args.name_space}"
+}
