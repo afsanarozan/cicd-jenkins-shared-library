862820039ffb8674d90b81aff990b78b51ae5ad3
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-08 09:41:19 +0700

    testing sonarube ee

--- vars/sonarqubeScanner.groovy
+++ vars/sonarqubeScanner.groovy
@@ -2,7 +2,7 @@ def call(Map envar) {
 def config = pipelineCfg()
         container('sonarscanner'){
         echo "Running Code Review With SonarQube"
-                withSonarQubeEnv(credentialsId: "sonarqube-token", installationName: 'sonarqube') {
+                withSonarQubeEnv(credentialsId: "sonarqube-ee-token", installationName: 'sonarqube') {
                 sonarScanGo(name_space: config.name_space, service_name: config.service_name, sonarSrc: config.sonarSrc)
                 }       
         }
@@ -10,6 +10,8 @@ def config = pipelineCfg()
 
 def sonarScanGo(Map args){
         sh " sonar-scanner -X \
+        -Dsonar.host.url=http://sonarqube-ee.toolchains.atklik.xyz \
+        -Dsonar.login=c13cec4b07228eab94869950ac5d972386168524 \
         -Dsonar.projectKey=${args.name_space}-${args.service_name} \
         -Dsonar.sources=${args.sonarSrc} \
         -Dsonar.qualitygate.wait=true \
