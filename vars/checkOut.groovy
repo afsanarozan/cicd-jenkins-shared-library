ed209237ea759601a961def939946b81ffff43e8
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-09 13:36:47 +0700

    edit checout variable

--- vars/checkOut.groovy
+++ vars/checkOut.groovy
@@ -22,7 +22,7 @@ def call(Map args) {
     // def branches = []
 
     echo "checkout branch ${config.branch}" 
-    checkout([$class: 'GitSCM', branches: [[name: "${config.branch}"]], userRemoteConfigs: [[credentialsId: "${config.credential}", url: "https://gitlab.com/kliklab/automation-platform/services-platform/${config.name_space}/${config.service_name}.git"]]])
+    checkout([$class: 'GitSCM', branches: [[name: "${config.branch}"]], userRemoteConfigs: [[credentialsId: "${config.credential}", url: "${config.repo_url}"]]])
     sh 'ls'
 }
 
