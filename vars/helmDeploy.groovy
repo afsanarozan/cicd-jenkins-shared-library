4e5626f6a5b44343043a5a220fb974f2904057d7
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-11 14:18:33 +0700

    testing debug

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -23,5 +23,12 @@ def helmUpgrade(Map args) {
 
 def helmInstall(Map args) {
     sh "cd ${args.service_name}"
+    sh "ls"
+    sh "cd ${args.service_name}"
+    sh "ls"
+    sh "cd ${args.service_name}"
+    sh "ls"
+    sh "cd ${args.service_name}"
+    sh "ls"
     sh "helm install ${args.service_name} . -f values.yaml -n ${args.name_space}"
 }
