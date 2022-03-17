d575224b964d6b1a86c4838ca65721e3cb0adefe
taupiknr26 <taupiknr26@gmail.com>
2022-03-18 00:33:25 +0700

    update library

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,15 +1,22 @@
-def call(Map envar) {
-    def config = pipelineCfg()
-    container('docker') {
-        echo "Running Docker Push"
-        echo "${config.container_registry}"
-        sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
-        dockerPush(registry_url: config.container_registry, image_name: config.image_name)
-    }        
+def call() {
+   def config = pipelineCfg()
+   def envar = checkoutCode()
+    if(envar.environment == 'dev' || envar.environment  == 'staging' || envar.environment  == 'production'){
+        container('docker') {
+            echo "Running Docker Push"
+            sh 'docker login ${setting.do_url} -u ${setting.do_token_registry} -p ${setting.do_token_registry}'
+            dockerPush(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion: "${envar.version}-${BUILD_NUMBER}")
+        }
+    }else{
+    skip()
+}
+}
+def dockerPushTag(Map args) {
+    sh "docker tag ${args.registry_url}/${args.image_name}:${args.srcVersion} ${args.registry_url}/${args.image_name}:${args.dstVersion}"
+    sh "docker push ${args.registry_url}/${args.image_name}:${args.dstVersion}"
+    sh "docker rmi ${args.registry_url}/${args.image_name}:${args.dstVersion}"
 }
 
-def dockerPush(Map args) {
-    sh "docker push ${args.registry_url}/${args.image_name}:beta"
-    sh "docker rmi ${args.registry_url}/${args.image_name}:beta"
+def skip(){
+    sh "echo '=============== SKIP =============='"
 }
-                        
