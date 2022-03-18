cbf3bc9460fc359e4a9721813a9b33f5f9c368ef
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-18 07:37:16 +0000

    Update dockerBuild2.groovy

--- vars/dockerBuild2.groovy
+++ vars/dockerBuild2.groovy
@@ -2,10 +2,16 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-  if(envar.environment == 'dev' || envar.environment  == 'staging' || envar.environment  == 'production'){
+  if(envar.environment == 'dev' || envar.environment  == 'staging'){
                     container('docker') {
                         echo "Running Docker Build"
                         dockerBuild(registry_url: config.url_images_registry, image_name: config.service_name, image_version: envar.version)
+                    }
+          }
+  if(envar.environment  == 'production'){
+                    container('docker') {
+                        echo "Running Docker Build"
+                        dockerBuild2(registry_url: config.url_images_registry, image_name: config.service_name, dstVersion: "${config.Tag}-${BUILD_NUMBER}")
                     }
           }else{
             skip()
@@ -17,6 +23,10 @@ def dockerBuild(Map args) {
   sh 'docker images'
 }      
 
+def dockerBuild2(Map args) {
+  sh "docker build -t ${args.registry_url}/${args.image_name}:${args.dstVersion} ."
+  sh 'docker images'
+}
 
 def skip(){
     sh "echo '=============== SKIP =============='"
