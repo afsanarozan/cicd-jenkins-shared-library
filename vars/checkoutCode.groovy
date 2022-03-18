8adeb96933d064f41384766d4383330fc02a130d
devops RND <automation@klik.digital>
2022-03-18 09:21:50 +0000

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -10,7 +10,7 @@ def call() {
         envar.branch      = "main"
         envar.environment = 'staging'
         envar.version     = "beta"
-      }else if(gitlabActionType == 'TAG_PUSH') {
+      }else if(env.gitlabActionType == 'TAG_PUSH') {
         envar.environment = 'production'
         envar.version     = "release"
         envar.branch      = env.gitlabSourceBranch
