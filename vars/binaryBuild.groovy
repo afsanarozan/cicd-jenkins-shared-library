4f4fd32aea9150a752411ff2072043dee3df5c3a
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-21 14:40:36 +0700

    edit command lina

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -1,19 +1,31 @@
 def call {
-    script {
-        sshPublisher(
-            publishers: [
-                sshPublisherDesc(
-                    configName: 'vm-do-testing',
-                    verbose: false,
-                    transfers: [
-                        sshTransfer(
-                            remoteDirectory: "/home",
-                            execCommand: "whoami",
-                            execTimeout: 60000
-                        )
-                    ]
-                )
-            ]
-        )
-    }
+    // script {
+    //     sshPublisher(
+    //         publishers: [
+    //             sshPublisherDesc(
+    //                 configName: 'vm-do-testing',
+    //                 verbose: false,
+    //                 transfers: [
+    //                     sshTransfer(
+    //                         remoteDirectory: "/home",
+    //                         execCommand: "whoami",
+    //                         execTimeout: 60000
+    //                     )
+    //                 ]
+    //             )
+    //         ]
+    //     )
+    // }
+    def remote = [:]
+        remote.name = 'vm-do-testing'
+        remote.host = '178.128.97.157'
+        remote.user = 'root'
+        remote.password = '2021klikLabs'
+        remote.allowAnyHosts = true
+    
+    sshDeploy(remote)
+}
+
+def sshDeploy(Map args){
+    sshCommand remote: remote, command: "ls -lrt"
 }
