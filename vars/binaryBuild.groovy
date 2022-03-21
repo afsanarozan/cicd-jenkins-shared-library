e087c31b7029bf7970d58ced0c26497eeb83bd0c
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-21 17:28:02 +0700

    testing build binary on vm

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -9,7 +9,7 @@ def call(Map args){
                     transfers: [
                         sshTransfer(
                             remoteDirectory: "/home",
-                            execCommand: "export PATH=$PATH:/usr/local/go/bin; go version",
+                            execCommand: "export PATH=$PATH:/usr/local/go/bin; git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git; cd example-service; git checkout binary-build-deployment; go mod download; go mod verify; go mod tidy -v; go build . ; ls",
                             execTimeout: 60000
                         )
                     ]
