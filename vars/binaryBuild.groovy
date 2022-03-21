0dc569adc0358439f1928730f5d56365308b64ec
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-21 14:26:26 +0700

    add binary build

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -1,17 +1,19 @@
 def call {
-    sshPublisher(
-        publishers: [
-            sshPublisherDesc(
-                configName: 'vm-do-testing',
-                verbose: false,
-                transfers: [
-                    sshTransfer(
-                        remoteDirectory: "/home",
-                        execCommand: "whoami",
-                        execTimeout: 60000,
-                    )
-                ]
-            )
-        ]
-    )
+    script {
+        sshPublisher(
+            publishers: [
+                sshPublisherDesc(
+                    configName: 'vm-do-testing',
+                    verbose: false,
+                    transfers: [
+                        sshTransfer(
+                            remoteDirectory: "/home",
+                            execCommand: "whoami",
+                            execTimeout: 60000,
+                        )
+                    ]
+                )
+            ]
+        )
+    }
 }
