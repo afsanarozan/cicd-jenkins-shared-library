31f434dc70e1617273756cc310751582b8f4ce02
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-23 10:39:54 +0700

    tes connection using ssh agent

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -1,51 +1,51 @@
 def call(Map args){
     def config = pipelineCfg()
-    script {
-        sshPublisher(
-            publishers: [
-                sshPublisherDesc(
-                    configName: 'vm-do-testing',
-                    verbose: true,
-                    transfers: [
-                        sshTransfer(
-                            remoteDirectory: "/home",
-                            execCommand: """
-                            export PATH=$PATH:/usr/local/go/bin;
-                            export AWS_ACCESS_KEY_ID=YFRP3PS4LIJEOZVRUMMK;
-                            export AWS_SECRET_ACCESS_KEY=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs;
-                            export AWS_DEFAULT_REGION=sgp1;
-                            git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git;
-                            cd example-service;
-                            git checkout binary-build-deployment;
-                            go version;
-                            go mod download;
-                            go mod verify;
-                            go mod tidy -v; 
-                            go build .;
-                            aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/
-                            """,
-                            execTimeout: 60000
-                        )
-                    ]
-                )
-            ]
-        )
-    }
+    // script {
+    //     sshPublisher(
+    //         publishers: [
+    //             sshPublisherDesc(
+    //                 configName: 'vm-do-testing',
+    //                 verbose: true,
+    //                 transfers: [
+    //                     sshTransfer(
+    //                         remoteDirectory: "/home",
+    //                         execCommand: """
+    //                         export PATH=$PATH:/usr/local/go/bin;
+    //                         export AWS_ACCESS_KEY_ID=YFRP3PS4LIJEOZVRUMMK;
+    //                         export AWS_SECRET_ACCESS_KEY=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs;
+    //                         export AWS_DEFAULT_REGION=sgp1;
+
+    //                         git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git;
+    //                         cd example-service;
+    //                         git checkout binary-build-deployment;
+    //                         go version;
+
+    //                         go mod download;
+    //                         go mod verify;
+    //                         go mod tidy -v; 
+    //                         go build .;
+
+    //                         aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/
+    //                         """,
+    //                         execTimeout: 60000  
+    //                     )
+    //                 ]
+    //             )
+    //         ]
+    //     )
+    // }
+
+    steps{
+        sshagent(credentials:['loadtest-server-ssh']){
+            sh """ 
+            ssh  -o StrictHostKeyChecking=no root@178.128.97.157 
+            "echo ${tes}"
+            """
+        }
+
     sh "echo binary build"
 }
 
-// def sshDeploy(Map args){
-//     sshCommand remote: remote, command: "ls -lrt"
-// }
-
-// def goEnv(){
-//     sh '''
-//     export PATH=$PATH:/usr/local/go/bin
-//     git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git
-//     cd example-service
-//     git checkout binary-build-deployment
-//     '''
-// }
 
 def pushChart(Map args) {
     echo "Push Chart"
