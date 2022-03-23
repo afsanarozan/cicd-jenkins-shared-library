0a2b3b0edb76faf702025beee5efc218d81ed4c3
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-23 11:03:36 +0700

    edit binary build

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -1,46 +1,39 @@
 def call(Map args){
     def config = pipelineCfg()
-    // script {
-    //     sshPublisher(
-    //         publishers: [
-    //             sshPublisherDesc(
-    //                 configName: 'vm-do-testing',
-    //                 verbose: true,
-    //                 transfers: [
-    //                     sshTransfer(
-    //                         remoteDirectory: "/home",
-    //                         execCommand: """
-    //                         export PATH=$PATH:/usr/local/go/bin;
-    //                         export AWS_ACCESS_KEY_ID=YFRP3PS4LIJEOZVRUMMK;
-    //                         export AWS_SECRET_ACCESS_KEY=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs;
-    //                         export AWS_DEFAULT_REGION=sgp1;
+    script {
+        sshPublisher(
+            publishers: [
+                sshPublisherDesc(
+                    configName: 'vm-do-testing',
+                    verbose: true,
+                    transfers: [
+                        sshTransfer(
+                            remoteDirectory: "/home",
+                            execCommand: """
+                            export PATH=$PATH:/usr/local/go/bin;
+                            export AWS_ACCESS_KEY_ID=YFRP3PS4LIJEOZVRUMMK;
+                            export AWS_SECRET_ACCESS_KEY=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs;
+                            export AWS_DEFAULT_REGION=sgp1;
 
-    //                         git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git;
-    //                         cd example-service;
-    //                         git checkout binary-build-deployment;
-    //                         go version;
+                            git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git;
+                            cd example-service;
+                            git checkout binary-build-deployment;
+                            go version;
 
-    //                         go mod download;
-    //                         go mod verify;
-    //                         go mod tidy -v; 
-    //                         go build .;
+                            go mod download;
+                            go mod verify;
+                            go mod tidy -v; 
+                            go build -o ${config.service_name}/${tag};
 
-    //                         aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/
-    //                         """,
-    //                         execTimeout: 60000  
-    //                     )
-    //                 ]
-    //             )
-    //         ]
-    //     )
-    // }
-        sshagent(credentials:['loadtest-server-ssh']){
-            sh """ 
-            ssh  -o StrictHostKeyChecking=no root@178.128.97.157 
-            "echo ${tes}"
-            """
-        }
-
+                            aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/beta
+                            """,
+                            execTimeout: 60000  
+                        )
+                    ]
+                )
+            ]
+        )
+    }
     sh "echo binary build"
 }
 
