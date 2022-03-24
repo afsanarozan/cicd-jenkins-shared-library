bfdd86e6876d237ea010f296ce705d56fe17c838
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-24 14:40:50 +0700

    add zipp

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -26,7 +26,7 @@ def call(Map args){
             
             ls -la
             
-            sudo apt install zip
+            apt install zip
             curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
             unzip awscliv2.zip
             sudo ./aws/install
