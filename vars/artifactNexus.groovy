ee49798387d226553e3e9134583f1fe21b1724ca
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-24 12:47:37 +0700

    add artifact nexus

--- vars/artifactNexus.groovy
+++ vars/artifactNexus.groovy
@@ -4,6 +4,7 @@ def call(Map Args) {
         checkout([$class: 'GitSCM', branches: [[name: "main"]], userRemoteConfigs: [[credentialsId: "6c8b6848-ca10-4862-b1a9-fe3e6d46da61", url: "https://gitlab.com/afsanarozanaufal/helm-chart"]]])
         sh 'ls'
         sh """
+        helm plugin install --version master https://github.com/sonatype-nexus-community/helm-nexus-push.git
         chmod 777 ./package.sh
         ./package.sh
         ls
