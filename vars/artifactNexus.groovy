148d9fa7e3af93e579d3026c9e81411b0869581b
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-24 12:41:01 +0700

    add artifact nexus

--- vars/artifactNexus.groovy
+++ vars/artifactNexus.groovy
@@ -2,7 +2,9 @@ def call(Map Args) {
     echo "checkout branch main" 
     checkout([$class: 'GitSCM', branches: [[name: "main"]], userRemoteConfigs: [[credentialsId: "6c8b6848-ca10-4862-b1a9-fe3e6d46da61", url: "https://gitlab.com/afsanarozanaufal/helm-chart"]]])
     sh 'ls'
+    sh """
     chmod 777 ./package.sh
     ./package.sh
     ls
+    """
 }
