b9e4034e9fd8a30fb5985e06c86aa4c0c7fd6f3a
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-24 04:55:01 +0000

    Add new file

---
+++ vars/mergeCode.groovy
@@ -0,0 +1,52 @@
+def call(){
+
+    def envar = checkoutCode()
+    sh "printenv | sort"
+
+     if(envar.environment == 'staging'){
+       source = "${env.gitlabSourceBranch}"
+       target   = "${env.gitlabTargetBranch}"
+       repo = "${env.gitlabSourceRepoHttpUrl}"
+       merge(source,target,repo)
+     
+     }else{
+         skip()
+     }
+
+   }
+    
+def merge(source,target,repo){
+      echo " Merge Request Detected!!  Merging"
+                    checkout ([
+                        $class: 'GitSCM',
+                        branches: [[name: "origin/${source}"]],
+                        extensions: [
+                            [$class: 'CleanCheckout'],
+                            [
+                                $class: 'PreBuildMerge',
+                                options: [
+                                    fastForwardMode: 'NO_FF',
+                                    mergeRemote: "origin",
+                                    mergeTarget: "${target}"
+                                ]
+                            ],
+                            [
+                                    $class: 'UserIdentity',
+                                    email: 'ysetiawan@xl.co.id',
+                                    name: 'ysetiawan'
+                                ]
+                        ],
+                                
+                        userRemoteConfigs: [[credentialsId: 'gitlab-auth-token', url: "${repo}"]]
+                    ])
+
+
+                withCredentials([usernamePassword(credentialsId: 'gitlab-auth-token', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                    sh "git pull https://${USERNAME}:${PASSWORD}@${repository} master"
+                    sh "git remote set-url origin https://${USERNAME}:${PASSWORD}@${repository}; git push origin HEAD:master"
+   }
+}
+
+def skip(){
+    sh "echo '=============== SKIP =============='"
+}
