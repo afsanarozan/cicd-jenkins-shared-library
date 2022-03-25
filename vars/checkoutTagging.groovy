2dbe70c42ea56e138a108ba378a67b4b2b3727c1
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-25 04:43:23 +0000

    Update checkoutTagging.groovy

--- vars/checkoutTagging.groovy
+++ vars/checkoutTagging.groovy
@@ -6,18 +6,18 @@ def call() {
   switch(env.BRANCH_NAME) {
   
     case 'main':
-      if (gitlabActionType == 'MERGE_REQUEST') {
+      if (env.gitlabActionType == 'MERGE_REQUEST') {
         envar.branch      = "main"
         envar.environment = 'staging'
         envar.version     = "beta"
-      }else if(gitlabActionType == 'TAG_PUSH') {
+      }else if(env.gitlabActionType == 'TAG_PUSH') {
         envar.environment = 'production'
         envar.version     = "release"
         envar.branch      = env.gitlabSourceBranch
       }
       break;
     case 'development':
-      if (gitlabActionType == 'PUSH') {
+      if (env.gitlabActionType == 'PUSH') {
         envar.branch      = "development"
         envar.environment = 'dev'
         envar.version     = "alpha"
