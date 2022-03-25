a8023a7832be5df726e9c940ab12227ae0fd412e
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-25 07:57:54 +0000

    Update checkoutCode.groovy

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -17,11 +17,9 @@ def call() {
       }
       break;
     default:
-         if(env.gitlabSourceBranch == "*/development"){
-            envar.branch      = "*/${env.BRANCH_NAME}"
-            envar.environment = 'dev'
-            envar.version     = "alpha"
-         }
+        envar.branch      = "*/${env.BRANCH_NAME}"
+        envar.environment = 'dev'
+        envar.version     = "alpha"
       break;
   }
 
