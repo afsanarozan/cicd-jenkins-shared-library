a095a91ec234b04b4a1f23a3d24a1ae092570324
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-28 09:12:52 +0700

    add binary build

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -1,20 +1,11 @@
 def call(Map args){
     def config = pipelineCfg()
+    sh "echo binary build"
     container ('golang'){
-        // checkout changelog: true, poll: true, scm: [
-        // $class: 'GitSCM',
-        // branches: [[name: "binary-build-deployment"]],
-        // doGenerateSubmoduleConfigurations: false,
-        // submoduleCfg: [],
-        // userRemoteConfigs: [[credentialsId: '6c8b6848-ca10-4862-b1a9-fe3e6d46da61', url: "${config.repo_url}"]]
-        // ]
         sh 'ls'
         sh """
-            export PATH=$PATH:/usr/local/go/bin
             go version
-
             git config --global url."https://afsanarozanaufal:glpat-fhyFdTnzjm-sQJ4epsXK@gitlab.com/kds-platform/plugin.git".insteadOf "https://gitlab.com/kds-platform/plugin.git"
-
             go mod download
             go mod verify
             go mod tidy -v 
@@ -26,41 +17,6 @@ def call(Map args){
     container('aws-cli'){
         sh "aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/beta"
     }
-    // script {
-    //     sshPublisher(
-    //         publishers: [
-    //             sshPublisherDesc(
-    //                 configName: 'vm-do-testing',
-    //                 verbose: true,
-    //                 transfers: [
-    //                     sshTransfer(
-    //                         remoteDirectory: "/home",
-    //                         execCommand: """
-    //                         export PATH=$PATH:/usr/local/go/bin;
-    //                         export AWS_ACCESS_KEY_ID=YFRP3PS4LIJEOZVRUMMK;
-    //                         export AWS_SECRET_ACCESS_KEY=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs;
-    //                         export AWS_DEFAULT_REGION=sgp1;
-
-    //                         git clone https://automation:glpat-Vf6rMnhFzEbshHrj2TYQ@gitlab.com/kliklab/automation-platform/services-platform/example-service.git;
-    //                         cd example-service;
-    //                         git checkout binary-build-deployment;
-    //                         go version;
-
-    //                         go mod download;
-    //                         go mod verify;
-    //                         go mod tidy -v; 
-    //                         go build -o ${config.service_name}-${config.tag};
-
-    //                         aws s3 cp ${config.service_name} --endpoint-url ${config.spaces_url} s3://binary-build/beta
-    //                         """,
-    //                         execTimeout: 60000  
-    //                     )
-    //                 ]
-    //             )
-    //         ]
-    //     )
-    // }
-    sh "echo binary build"
 }
 
 
