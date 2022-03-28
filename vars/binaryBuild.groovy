709ba81b7c04b21368bc2c3c7b2f19e6daba84ab
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-28 08:43:46 +0700

    add wget download link go

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -10,6 +10,7 @@ def call(Map args){
         ]
         sh 'ls'
         sh """
+            wget https://dl.google.com/go/go1.18.linux-amd64.tar.gz
             tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
             export PATH=$PATH:/usr/local/go/bin
             go version
