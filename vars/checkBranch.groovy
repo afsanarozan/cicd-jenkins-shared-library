ab81af49df81d10c59737e2f3beaf5e4f33424cc
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-30 14:56:14 +0700

    edit message

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -1,16 +1,16 @@
 def call() {
-  def config = pipelineCfg()
-  def envar = checkoutCode()
+    def config = pipelineCfg()
+    def envar = checkoutCode()
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
   print("ini :" + envar)
     if (env.gitlabSourceBranch == 'development'){
         echo "passed"
     } else {
         echo "job success"
-            def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-            def summary = "${subject} (${env.BUILD_URL})"
-
             // Send notifications
-            slackSend (color: '#00FF00', message: summary)
+            slackSend (color: '#00FF00', message: "tes")
         error "This pipeline stops here!"
     }       
 }
