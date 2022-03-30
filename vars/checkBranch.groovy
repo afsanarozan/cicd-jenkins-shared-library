552e439697a304e91434627ae55c37e1580312e7
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-30 14:43:59 +0700

    add check branch notification

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -8,20 +8,27 @@ def call() {
         echo "job success"
         skip()
         error "This pipeline stops here!"
-    }
-
-//   if(envar.branch == '*/development' || envar.environment  == 'staging'){
-//             container('docker') {
-//                 echo "Running Docker Build"
-//             }
-//         }
-//   if(envar.environment  == 'production'){
-//             container('docker') {
-//                 echo "Running Docker Build"
-//             }
-//         }        
+    }       
 }
 
 def skip(){
-    def envar = slackNotification()
+    // def envar = slackNotification()
+      // Default values
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESS') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+        colorCode = '#FF0000'
+    }
+
+    // Send notifications
+    slackSend (color: '#00FF00', message: summary)
 }
