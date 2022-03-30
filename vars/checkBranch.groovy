ff3a2e31f238a3c79e7b7b2eb010fb25574f56c4
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-30 04:05:10 +0000

    Add new file

---
+++ vars/checkBranch.groovy
@@ -0,0 +1,17 @@
+def call() {
+  def config = pipelineCfg()
+  def envar = checkoutCode()
+  print("ini :" + envar)
+  if(envar.branch == '*/development' || envar.environment  == 'staging'){
+                    container('docker') {
+                        echo "Running Docker Build"
+                    }
+          }
+  if(envar.environment  == 'production'){
+                    container('docker') {
+                        echo "Running Docker Build"
+                    }
+          }else{
+            error "This pipeline stops here!"          
+          }        
+}
