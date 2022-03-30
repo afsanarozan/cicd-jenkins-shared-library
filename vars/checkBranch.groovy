3f57a6d3f3ed4ead92ba3dd561e348cb5125d80d
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-30 14:45:17 +0700

    edit check branch notification

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -6,7 +6,11 @@ def call() {
         echo "passed"
     } else {
         echo "job success"
-        skip()
+            def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+            def summary = "${subject} (${env.BUILD_URL})"
+
+            // Send notifications
+            slackSend (color: '#00FF00', message: summary)
         error "This pipeline stops here!"
     }       
 }
@@ -14,21 +18,5 @@ def call() {
 def skip(){
     // def envar = slackNotification()
       // Default values
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    def summary = "${subject} (${env.BUILD_URL})"
-
-    // Override default values based on build status
-    if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-    } else if (buildStatus == 'SUCCESS') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
-    } else {
-        color = 'RED'
-        colorCode = '#FF0000'
-    }
-
-    // Send notifications
-    slackSend (color: '#00FF00', message: summary)
+   
 }
