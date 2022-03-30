dd123af3f96c9fb561e039558eef6cbc0a98cea7
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-30 13:48:32 +0700

    edit logic

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -2,21 +2,22 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-  if(envar.branch == '*/development' || envar.environment  == 'staging'){
-                    container('docker') {
-                        echo "Running Docker Build"
-                    }
-          }
-  if(envar.environment  == 'production'){
-                    container('docker') {
-                        echo "Running Docker Build"
-                    }
-          }
-  if(envar.branch != '*/development' || envar.environment  != 'staging' || envar.environment  != 'production'){
-             container('docker') {
-            skip()
-             }
-          }        
+  if(env.gitlabSourceBranch == 'development'){
+        echo "passed"
+    } else {
+        skip()
+    }
+
+//   if(envar.branch == '*/development' || envar.environment  == 'staging'){
+//             container('docker') {
+//                 echo "Running Docker Build"
+//             }
+//         }
+//   if(envar.environment  == 'production'){
+//             container('docker') {
+//                 echo "Running Docker Build"
+//             }
+//         }        
 }
 
 def skip(){
