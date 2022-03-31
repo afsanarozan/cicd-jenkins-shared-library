e723480d13dddcbdcaac58369e828faaa594270e
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-31 05:55:05 +0000

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,33 @@
-def call(Map envar) {
-      def config = pipelineCfg()
-        container('docker'){
-            echo "Running Docker Build"
-            dockerBuild(container_registry: config.container_registry, image_name: config.image_name, version: config.tag)
-        }
-} 
-
-def dockerBuild(Map args) {
-    sh "docker build -t ${args.container_registry}/${args.image_name}:${args.version} ."
+def call() {
+  def config = pipelineCfg()
+  def envar = checkoutCode()
+  print("ini :" + envar)
+  if( envar.environment  == 'dev' || envar.environment  == 'staging'){
+                    container('docker') {
+                        echo "Running Docker Build"
+                        dockerBuild(registry_url: config.url_images_registry, image_name: config.service_name, image_version: "${envar.version}-${BUILD_NUMBER}")
+                    }
+          }
+  if(envar.environment  == 'production'){
+                    container('docker') {
+                        echo "Running Docker Build"
+                        dockerBuild2(registry_url: config.url_images_registry, image_name: config.service_name, dstVersion: "${config.Tag}-${BUILD_NUMBER}")
+                    }
+          }else{
+            skip()           
+          }        
 }
 
+def dockerBuild(Map args) {
+  sh "docker build -t ${args.registry_url}/${args.image_name}:${args.image_version} ."
+  sh 'docker images'
+}      
+
+def dockerBuild2(Map args) {
+  sh "docker build -t ${args.registry_url}/${args.image_name}:${args.dstVersion} ."
+  sh 'docker images'
+}
+
+def skip(){
+    sh "echo '=============== SKIP =============='"
+}
