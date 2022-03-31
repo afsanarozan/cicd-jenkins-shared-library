c711be91e4bd9ce8fb059b48b1b77df2308ab71f
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-31 11:52:15 +0700

    edit binary build step

--- vars/checkBranch.groovy
+++ vars/checkBranch.groovy
@@ -2,7 +2,7 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-    if (env.gitlabSourceBranch == 'development'){
+    if (env.BRANCH_NAME == 'development'){
         echo "passed"
     } else {
         echo "job success"
