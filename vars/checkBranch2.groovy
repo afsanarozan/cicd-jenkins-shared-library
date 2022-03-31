17087453adedae5c46cd7418131f1ed28a3dbac1
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-31 04:47:29 +0000

    Update checkBranch2.groovy

--- vars/checkBranch2.groovy
+++ vars/checkBranch2.groovy
@@ -2,7 +2,7 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-    if (env.gitlabSourceBranch == '*/development'){
+    if (envar.branch == '*/development'){
         echo "passed"
     } else {
         echo "job success"
