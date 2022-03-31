17d2945c96fa412a821402fb7aee795ed51169bc
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-31 09:37:06 +0700

    edit binary build step

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -8,7 +8,7 @@ def call(Map args){
             buildBinary(service_name: config.service_name, version: envar.version)
         }
         container('aws-cli'){
-            pushBinary(service_name: config.service_name, spaces_url: config.spaces_url, namespace: config.name_space)
+            pushBinary(service_name: config.service_name, spaces_url: config.spaces_url, environment: envar.environment)
         }
     }
 }
@@ -25,5 +25,5 @@ def buildBinary(Map args) {
 
 def pushBinary(Map args) {
     echo "Push Chart"
-    sh "aws s3 cp ${args.service_name} --endpoint-url ${args.spaces_url} s3://binary-build/${args.namespace}/"
+    sh "aws s3 cp ${args.service_name} --endpoint-url ${args.spaces_url} s3://binary-build/${args.environment}/"
 }
