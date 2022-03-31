4ad8ec9b67392b7dcba082542240a406960fdc5a
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-31 05:56:15 +0000

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,31 +1,33 @@
 def call() {
-  def config = pipelineCfg()
-  def envar = checkoutCode()
-  print("ini :" + envar)
-  if( envar.environment  == 'dev' || envar.environment  == 'staging'){
-                    container('docker') {
-                        echo "Running Docker Build"
-                        dockerBuild(registry_url: config.url_images_registry, image_name: config.service_name, image_version: "${envar.version}-${BUILD_NUMBER}")
-                    }
-          }
-  if(envar.environment  == 'production'){
-                    container('docker') {
-                        echo "Running Docker Build"
-                        dockerBuild2(registry_url: config.url_images_registry, image_name: config.service_name, dstVersion: "${config.Tag}-${BUILD_NUMBER}")
-                    }
-          }else{
-            skip()           
-          }        
+   def config = pipelineCfg()
+   def envar = checkoutCode()
+    if(envar.branch == '*/development' || envar.environment  == 'staging'){
+        container('docker') {
+            echo "Running Docker Push"
+            sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
+            dockerPush(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion: "${envar.version}-${BUILD_NUMBER}")
+        }
+    }
+    if(envar.environment  == 'production'){
+        container('docker') {
+            echo "Running Docker Push"
+            sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
+            dockerPush2(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion2: "${config.Tag}-${BUILD_NUMBER}")
+        }
+    }else{
+    skip()
+}
+}
+def dockerPush(Map args) {
+    sh "docker tag ${args.registry_url}/${args.image_name}:${args.srcVersion} ${args.registry_url}/${args.image_name}:${args.dstVersion}"
+    sh "docker push ${args.registry_url}/${args.image_name}:${args.dstVersion}"
+    sh "docker rmi ${args.registry_url}/${args.image_name}:${args.dstVersion}"
 }
 
-def dockerBuild(Map args) {
-  sh "docker build -t ${args.registry_url}/${args.image_name}:${args.image_version} ."
-  sh 'docker images'
-}      
-
-def dockerBuild2(Map args) {
-  sh "docker build -t ${args.registry_url}/${args.image_name}:${args.dstVersion} ."
-  sh 'docker images'
+def dockerPush2(Map args) {
+    sh "docker tag ${args.registry_url}/${args.image_name}:${args.dstVersion2} ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
+    sh "docker push ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
+    sh "docker rmi ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
 }
 
 def skip(){
