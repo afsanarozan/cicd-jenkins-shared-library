89738665a2e950feb2c357ad3c1c55335cfc2a72
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-03-31 13:38:53 +0000

    Update emailNotification.groovy

--- vars/emailNotification.groovy
+++ vars/emailNotification.groovy
@@ -3,26 +3,24 @@
 /**
  * Send notifications based on build status string
  */
-def call(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  buildStatus = buildStatus ?: 'SUCCESS'
-
+def call(String currentBuild.result) {
+  
   // Default values
   def colorName = 'RED'
   def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def subject = "${currentBuild.result}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+  def details = """<p>${currentBuild.result}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
   // Override default values based on build status
-  if (buildStatus == 'STARTED') {
+  if (currentBuild.result == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
+  } else if (currentBuild.result == 'SUCCESS') {
     color = 'GREEN'
     colorCode = '#00FF00'
-  } else {
+  } else if (currentBuild.result == 'FALSE'){
     color = 'RED'
     colorCode = '#FF0000'
   }
