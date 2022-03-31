c431b563f9b8a0f29d656a00386a34028c085b4a
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-03-31 13:34:21 +0700

    edit url helm nexus and rename it

--- vars/finterlabsArtifact.groovy
+++ vars/finterlabsArtifact.groovy
@@ -1,7 +1,7 @@
 def call(Map Args) {
     container('base') {
     echo "checkout branch main" 
-        checkout([$class: 'GitSCM', branches: [[name: "main"]], userRemoteConfigs: [[credentialsId: "6c8b6848-ca10-4862-b1a9-fe3e6d46da61", url: "https://gitlab.com/afsanarozanaufal/helm-chart"]]])
+        checkout([$class: 'GitSCM', branches: [[name: "main"]], userRemoteConfigs: [[credentialsId: "6c8b6848-ca10-4862-b1a9-fe3e6d46da61", url: "https://gitlab.com/jeramnet/helm-chart.git"]]])
         installPlugin()
         sh """
         chmod 777 ./package.sh
