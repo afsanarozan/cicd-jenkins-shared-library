ba5cbb1cdb9e32694cbb24309a43506eb37cebe3
taupiknr26 <taupiknr26@gmail.com>
2022-04-01 19:43:04 +0700

    update dockerpush

---
+++ backup/revertGitlab.groovy
@@ -0,0 +1,11 @@
+def call() {
+    echo "last id merge:${env.gitlabMergeRequestLastCommit}"
+
+container('curl') {
+     withCredentials([string(credentialsId: 'god-taupik-key', variable: 'PRIVATE_TOKEN')]) {
+        echo "trigger gitlab revert"
+        
+         sh "curl -H \"PRIVATE-TOKEN:\$PRIVATE_TOKEN\" -X POST \"https://gitlab.com/api/v4/projects/${env.gitlabMergeRequestTargetProjectId}/repository/commits/${env.gitlabMergeRequestLastCommit}/revert\" --form branch=${env.gitlabTargetBranch}"
+    }
+}
+}
--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,8 @@
 def call() {
    def config = pipelineCfg()
    def envar = checkoutCode()
+    envar.tag = sh "git tag | head -1"
+    echo "test ${envar.tag}"
     if(envar.branch == '*/development' || envar.environment  == 'staging'){
         container('docker') {
             echo "Running Docker Push"
@@ -12,7 +14,7 @@ def call() {
         container('docker') {
             echo "Running Docker Push"
             sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
-            dockerPush2(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion2: "${config.Tag}-${BUILD_NUMBER}")
+            dockerPush2(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion2: envar.tag, latestVersion: config.image_latest)
         }
     }else{
     skip()
@@ -29,8 +31,10 @@ def dockerPush(Map args) {
     sh "docker rmi ${args.registry_url}/${args.image_name}:${args.latestVersion}"
 }
 
+
 def dockerPush2(Map args) {
-    sh "docker tag ${args.registry_url}/${args.image_name}:${args.srcVersion2} ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
+    sh "docker pull ${args.registry_url}/${args.image_name}:${args.latestVersion}"
+    sh "docker tag ${args.registry_url}/${args.image_name}:${args.latestVersion} ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
     sh "docker push ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
     sh "docker rmi ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
 }
