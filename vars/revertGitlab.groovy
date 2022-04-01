be83bfed257465bc1300a7aeb8ae065cad3f73aa
david <david.staliat@gmail.com>
2022-04-01 16:00:27 +0700

    test 2

--- vars/revertGitlab.groovy
+++ vars/revertGitlab.groovy
@@ -2,7 +2,7 @@ def call() {
     echo "last id merge:${env.gitlabMergeRequestLastCommit}"
 
 container('curl') {
-     withCredentials([string(credentialsId: 'global-gitlab-rnd', variable: 'PRIVATE_TOKEN')]) {
+     withCredentials([string(credentialsId: '7890dfab-50c8-4b50-a26a-772d5a5d7917', variable: 'PRIVATE_TOKEN')]) {
         echo "trigger gitlab revert"
         
          sh "curl -H \"PRIVATE_TOKEN:\$PRIVATE_TOKEN\" -X POST \"https://gitlab.com/api/v4/projects/${env.gitlabMergeRequestTargetProjectId}/repository/commits/:${env.gitlabMergeRequestLastCommit}/revert\" --form branch=${env.gitlabTargetBranch}"
