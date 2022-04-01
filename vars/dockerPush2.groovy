e6b5033fafb064b6dc5695c87af4f131160844af
Ade Kurniawan <adekuniawan1999@gmail.com>
2022-04-01 04:19:28 +0000

    Update dockerPush2.groovy

--- vars/dockerPush2.groovy
+++ vars/dockerPush2.groovy
@@ -5,7 +5,7 @@ def call() {
         container('docker') {
             echo "Running Docker Push"
             sh 'docker login registry.digitalocean.com -u d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b -p d5436ff5c04a514a24a86ac0415b7758a0ee3449835ad496fc516b61b2504f0b'
-            dockerPush2(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion2: "${config.Tag}-${BUILD_NUMBER}")
+            dockerPush2(registry_url: config.url_images_registry, image_name: config.service_name, srcVersion: envar.version, dstVersion2: "${config.Tag}--${BUILD_NUMBER}")
         }
     }else{
     skip()
@@ -13,7 +13,7 @@ def call() {
 }
 
 def dockerPush2(Map args) {
-    sh "docker pull ${args.registry_url}/${args.image_name}:latest"
+    sh "docker pull ${args.registry_url}/${args.image_name}:${args.dstVersion2} | head -1"
     sh "docker images"
     //sh "docker tag ${args.registry_url}/${args.image_name}:${args.dstVersion2} ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
     //sh "docker push ${args.registry_url}/${args.image_name}:${args.dstVersion2}"
