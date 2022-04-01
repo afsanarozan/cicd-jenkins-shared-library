6c08387b0cba46db6cefd8466950ed63e3c27490
taupiknr26 <taupiknr26@gmail.com>
2022-04-01 19:48:29 +0700

    update dockerpush

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,7 @@
 def call() {
    def config = pipelineCfg()
    def envar = checkoutCode()
-    envar.tag = sh "git tag | head -1"
+    envar.tag = sh(script: 'git tag | head -1 ', returnStdout: true)
     echo "test ${envar.tag}"
     if(envar.branch == '*/development' || envar.environment  == 'staging'){
         container('docker') {
