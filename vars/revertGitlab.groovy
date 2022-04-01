d3d6cf90a692fff6e56f474e6e0c4c64ce59b172
david <david.staliat@gmail.com>
2022-04-01 16:34:24 +0700

    remove titik dua

--- vars/revertGitlab.groovy
+++ vars/revertGitlab.groovy
@@ -5,7 +5,7 @@ container('curl') {
      withCredentials([string(credentialsId: 'god-taupik-key', variable: 'PRIVATE_TOKEN')]) {
         echo "trigger gitlab revert"
         
-         sh "curl -H \"PRIVATE-TOKEN:\$PRIVATE_TOKEN\" -X POST \"https://gitlab.com/api/v4/projects/${env.gitlabMergeRequestTargetProjectId}/repository/commits/:${env.gitlabMergeRequestLastCommit}/revert\" --form branch=${env.gitlabTargetBranch}"
+         sh "curl -H \"PRIVATE-TOKEN:\$PRIVATE_TOKEN\" -X POST \"https://gitlab.com/api/v4/projects/${env.gitlabMergeRequestTargetProjectId}/repository/commits/${env.gitlabMergeRequestLastCommit}/revert\" --form branch=${env.gitlabTargetBranch}"
     }
 }
 }
