d8f9952079ff1964a80173385390233aafc02dd0
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-06 09:54:20 +0700

    init deploy finterlabs platform

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -15,7 +15,9 @@ def helmFinterlabs(Map args) {
     sh """
     ls
     kubectl get ns
-    helm upgrade ${args.service_name} --install Charts/${args.service_name} -f ${values} -n ${args.name_space} --set image.tag=${env}-${BUILD_NUMBER}
+    helm repo add helm-finterlabs https://artifactory.finterlabs.com/repository/finterlabs-helm-local/ --username admin --password @klik123
+    helm repo update
+    helm upgrade --install kafka helm-finterlabs/kafka -f values/kafka.yaml -n testing --set image.tag=${env}-${BUILD_NUMBER}
     """
 }
 
