d9db5ddeb9996862ee4ebc2800dca957365bab74
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-11 09:01:19 +0700

    testing for debug

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -1,5 +1,6 @@
 def call() {
   sh "ls -lrth ${WORKSPACE}/"
+  sh "whoami"
   sh "cat ${WORKSPACE}/pipeline.yaml"
   Map pipelineCfg = readYaml(file: "${WORKSPACE}/pipeline.yaml")
   return pipelineCfg
