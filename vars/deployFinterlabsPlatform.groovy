abb1c45ee4468d8b487b0a7c73cb13c0b548584e
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-12 15:46:28 +0700

    tes

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -31,8 +31,6 @@ def installCli(){
         wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
         chmod a+x /usr/local/bin/yq
         yq --version
-
-        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     """
 }
 
