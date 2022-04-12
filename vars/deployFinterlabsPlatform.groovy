4dd6fc34328a979710f24beba45e011a984d96c2
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-12 15:45:09 +0700

    tes

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -33,6 +33,7 @@ def installCli(){
         chmod a+x /usr/local/bin/yq
         yq --version
 
+        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     """
 }
 
