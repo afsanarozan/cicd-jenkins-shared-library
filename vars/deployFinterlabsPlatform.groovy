b6fd18affb58994fca747018d712b5259f0147f0
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-12 09:16:24 +0700

    edit testing deploy platform

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -2,13 +2,31 @@ def call() {
   
   sh "printenv | sort"
   echo "Let's Deploy Platform"
-
-    container('base'){
+  sh ""
+    container('ubuntu') {
+        installCli()
+        
         withKubeConfig([credentialsId: "credential_tapera_dev_ali"]) {
-            helmFinterlabs()
+            // helmFinterlabs()
+            sh "cat ${credentialsId}"
         }
-    }       
+    }
+    // container('base'){
+        
+    // }       
 
+}
+
+def installCli(){
+    sh """
+        sudo apt-get upgrade
+        curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+        sudo apt-get install apt-transport-https --yes
+        echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+        sudo apt-get update
+        sudo apt-get install helm
+        sudo snap install yq
+    """
 }
 
 def helmFinterlabs(Map args) {
