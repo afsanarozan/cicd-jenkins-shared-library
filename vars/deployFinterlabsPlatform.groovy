5fe9d08b767dd58d95ebb30318c426c6af97d3f4
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-12 09:55:49 +0700

    edit deployFinterlabsPlatform

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -22,9 +22,10 @@ def installCli(){
         whoami
         apt-get upgrade
         apt-get update 
-        apt-get install snapd -y
-        snap install helm --classic
-        snap install yq
+        wget https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz
+        tar -zxvf helm-v3.8.0-linux-amd64.tar.gz
+        mv linux-amd64/helm /usr/local/bin/helm
+        helm version
     """
 }
 
