d94c2bfbcb32f4189b4b66ae12b806ca200f8c54
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-12 09:33:01 +0700

    edit deployFinterlabsPlatform

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -21,13 +21,13 @@ def installCli(){
     sh """
         whoami
         apt-get upgrade
-        apt-get update
-        apt-get install curl
+        apt-get update 
+        apt-get install curl -y
         curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
         apt-get install apt-transport-https --yes
         echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
         apt-get update
-        apt-get install helm
+        apt-get install helm -y
         snap install yq
     """
 }
