c780465802093e01da6e4ae63cafb360c9cd49aa
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-13 10:10:35 +0700

    edit if else on deployment

--- vars/deployFinterlabsPlatform.groovy
+++ vars/deployFinterlabsPlatform.groovy
@@ -47,7 +47,6 @@ def installCli(){
 def deployPlatform(Map args) {
     sh """
     ls
-    ./config/finterlabs-env.sh  
     helm repo add helm-finterlabs https://artifactory.finterlabs.com/repository/finterlabs-helm-local/ --username ${HELM_USER} --password ${HELM_PASSWORD}
     helm repo update
     helm upgrade --install ${env.platform} helm-finterlabs/${env.platform} -n finterlabs-platform
