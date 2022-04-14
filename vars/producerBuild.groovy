fd487dbc8e1b83a84ab38c689e1af8679b13a44f
david <david.staliat@gmail.com>
2022-04-14 14:45:36 +0700

    fixing docker command

--- vars/producerBuild.groovy
+++ vars/producerBuild.groovy
@@ -1,13 +1,13 @@
 def call(String service) {
     sh "cd ${service}"
     String name = "${env.DOCKER_FLABS}/${env.GROUP_IMAGE}/${env.GROUP_NAME}-${service}:${env.IMAGE_TAG}"
-    DockerBuilder(name)
+    dockerBuilder(name)
     sh 'cd ..'
 }
 
-def DockerBuilder(String name) {
+def dockerBuilder(String name) {
     container('docker') {
-        sh "docker built -t ${name} ."
+        sh "docker build -t ${name} ."
         sh "docker push ${name}"
     }
 }
