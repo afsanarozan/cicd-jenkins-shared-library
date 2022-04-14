39cf9221ce7d818b01c50861b011cce72035b39a
david <david.staliat@gmail.com>
2022-04-14 14:39:30 +0700

    fixing variable name

--- vars/producerBuild.groovy
+++ vars/producerBuild.groovy
@@ -1,11 +1,11 @@
-def call(service String) {
+def call(String service) {
     sh "cd ${service}"
     String name = "${env.DOCKER_FLABS}/${env.GROUP_IMAGE}/${env.GROUP_NAME}-${service}:${env.IMAGE_TAG}"
     DockerBuilder(name)
     sh 'cd ..'
 }
 
-def DockerBuilder(name String){
+def DockerBuilder(String name) {
     container('docker') {
         sh "docker built -t ${name} ."
         sh "docker push ${name}"
