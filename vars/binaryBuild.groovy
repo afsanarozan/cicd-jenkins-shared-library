730d51f73ef6f3a6788fdbde2979cead23a2595f
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-19 16:15:42 +0700

    testing binary build

--- vars/binaryBuild.groovy
+++ vars/binaryBuild.groovy
@@ -19,7 +19,7 @@ def call(Map args){
             buildBinary(service_name: config.service_name, version: envar.version, )
         }
         container('aws-cli'){
-            pushBinary(service_name: config.service_name, spaces_url: config.spaces_url, environment: envar.environment, application_name: envar.application_name)
+            pushBinary(service_name: config.service_name, spaces_url: config.spaces_url, environment: envar.environment, application_name: config.application_name, dstVersion: "${BUILD_NUMBER}")
         }
     }
 }
@@ -37,5 +37,6 @@ def buildBinary(Map args) {
 
 def pushBinary(Map args) {
     echo "Push Chart"
-    sh "aws s3 cp ${args.service_name} --endpoint-url ${args.spaces_url} s3://binary-build/${args.application_name}/${args.environment}/"
+    //sh "aws s3 cp s3://binary-build/${args.application_name}/${args.environment}/${args.service_name} --endpoint-url ${args.spaces_url} s3://binary-build/${args.application_name}/${args.environment}/${args.service_name}-${args.dstVersion}"
+    sh "aws s3 cp ${args.service_name} --endpoint-url ${args.spaces_url} s3://binary-build/${args.application_name}/${args.environment}/${args.service_name}"
 }
