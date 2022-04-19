7663d806dd94107075a6ad02286d85bd28cfda22
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-19 17:12:09 +0700

    add envar.environment

--- vars/pushChart.groovy
+++ vars/pushChart.groovy
@@ -14,13 +14,13 @@ def call(Map envar) {
     dir('Charts') {
         sh "ls"
             container('aws-cli'){
-            pushChart(service_name: config.service_name, spaces_url: config.spaces_url, namespace: config.name_space, dstVersion: "${BUILD_NUMBER}")
+            pushChart(service_name: config.service_name, spaces_url: config.spaces_url, namespace: config.name_space,environment: envar.environment, dstVersion: "${BUILD_NUMBER}")
         }
     }
 }
 
 def pushChart(Map args) {
     echo "Push Chart"
-    sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.namespace}/${envar.environment}/${args.service_name}-${args.dstVersion}.tgz"
+    sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.namespace}/${args.environment}/${args.service_name}-${args.dstVersion}.tgz"
     // sh "aws s3 cp s3://helm-charts/${args.namespace}/beta/${args.service_name}-*.tgz s3://helm-charts/${args.namespace}/beta/${args.service_name}-${args.dstVersion}.tgz"
 }
