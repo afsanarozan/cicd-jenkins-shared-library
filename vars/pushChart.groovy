82a880d18b13305b393ed027da0780c762b414f2
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-19 12:42:07 +0700

    edit push chart

--- vars/pushChart.groovy
+++ vars/pushChart.groovy
@@ -6,7 +6,7 @@ def call(Map envar) {
     dir('Charts') {
         sh "ls"
             container('aws-cli'){
-            pushChart(service_name: config.service_name, spaces_url: config.spaces_url, namespace: config.name_space, dstVersion: "${BUILD-NUMBER}")
+            pushChart(service_name: config.service_name, spaces_url: config.spaces_url, namespace: config.name_space, dstVersion: "${BUILD_NUMBER}")
         }
     }
 }
@@ -14,4 +14,5 @@ def call(Map envar) {
 def pushChart(Map args) {
     echo "Push Chart"
     sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.namespace}/beta/${args.service_name}-${args.dstVersion}.tgz"
+    // sh "aws s3 cp s3://helm-charts/${args.namespace}/beta/${args.service_name}-*.tgz s3://helm-charts/${args.namespace}/beta/${args.service_name}-${args.dstVersion}.tgz"
 }
