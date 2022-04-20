ae0ecd7f72feaae0be4ce07e14f48d7881e3a398
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-20 13:03:08 +0700

    solve issue unit test

--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -72,7 +72,7 @@ def goEnv() {
         git config --global url."https://afsanarozanaufal:glpat-fhyFdTnzjm-sQJ4epsXK@gitlab.com/kliklab/libs.git".insteadOf "https://gitlab.com/kliklab/libs.git"
         
         go get -u github.com/hashicorp/vault/api@v1.5.0
-        go build -mod=vendor
+        go build -mod=mod
         go mod vendor
         go mod tidy -v
 
