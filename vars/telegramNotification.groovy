4037e80a1eb2637e672beb277290c3c403673de4
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-26 08:52:20 +0700

    test notification

--- vars/telegramNotification.groovy
+++ vars/telegramNotification.groovy
@@ -1,4 +1,4 @@
-def call(String buildStatus = 'STARTED') {
+def call(String buildStatus) {
     def config = pipelineCfg()
 
     def telegram_chatid = -784775712
@@ -8,9 +8,8 @@ def call(String buildStatus = 'STARTED') {
     container('curl'){  
         if (buildStatus == 'STARTED') {
             notificationsStarted(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
-            echo "${buildStatus}"
         } else if (buildStatus == 'SUCCESS') {
-            notifications(telegram_url: config.telegram_url, telegram_chatid: config.telegram_chatid, job: env.JOB_NAME, job_numb: env.BUILD_NUMBER, job_url: env.BUILD_URL)
+            notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
             echo "${buildStatus}"
         } else if (buildStatus == 'FAILURE'){
             echo "passed"
@@ -29,12 +28,14 @@ def notificationsStarted (Map args) {
 }
 def notifications(Map args) {
     def message = "CICD Pipeline ${args.job} SUCCESS with build ${args.job_numb} \n\n More info at: ${args.job_url} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
-    parallel(
-        "Telegram": {
-            sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
-        },
-        "Jira": {
-            //jiraSend color: "${args.jira_url}", message: "${message}", channel: "${args.slack_channel}"
-        }
-    )
+    
+    sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
+    // parallel(
+    //     "Telegram": {
+    //         sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
+    //     },
+    //     "Jira": {
+    //         //jiraSend color: "${args.jira_url}", message: "${message}", channel: "${args.slack_channel}"
+    //     }
+    // )
 }
