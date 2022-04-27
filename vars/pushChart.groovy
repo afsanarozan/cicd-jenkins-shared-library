74fb2103959e5bc039b673b15ad629e71d11ec99
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-27 15:42:39 +0700

    add testing

--- vars/pushChart.groovy
+++ vars/pushChart.groovy
@@ -13,7 +13,7 @@ def call(Map args) {
     dir('Charts') {
         sh "ls -lah"
         container('s3cmd'){
-            sh "s3cmd --configure --access_key=YFRP3PS4LIJEOZVRUMMK --secret_key=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs --region=sgp1 --host=sgp1.digitaloceanspaces.com -s --no-encrypt --dump-config 2>&1 | tee .s3cfg"
+            sh "s3cmd --configure --access_key=YFRP3PS4LIJEOZVRUMMK --secret_key=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs --region=sgp1 --host=sgp1.digitaloceanspaces.com -s --no-encrypt --dump-config 2>&1 | tee /root/.s3cfg"
             sh "s3cmd get s3://labs-storage/helm-charts/api-gateway/alpha/reporting-service-0.1.0.tgz" 
             sh "ls -la"
         }
