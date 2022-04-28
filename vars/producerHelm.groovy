cc5cec132acb799c9b3ebc22357a047e305ae99e
david <david.staliat@gmail.com>
2022-04-28 14:06:45 +0700

    fixing helm repo url

--- vars/producerHelm.groovy
+++ vars/producerHelm.groovy
@@ -1,5 +1,5 @@
 def call() {
-    def chartdir = sh(
+    chartdir = sh(
         returnStdout: true,
         script: 'find * -maxdepth 2 -type d | grep -iF charts/')
         .trim().split('\r?\n')
@@ -13,9 +13,11 @@ def call() {
             credentialsId: 'artifactory-finterlabs',
             usernameVariable: 'NEXUS_USERNAME',
             passwordVariable: 'NEXUS_PASSWORD')]) {
-            sh 'helm repo add helm-private-repo $HELM_NEXUSREPO --username $NEXUS_USERNAME --password $NEXUS_PASSWORD'
-
             for (f in chartdir) {
+                lastpath = f.split('/').last()
+                sh """helm repo add
+                    ${lastpath} \$HELM_NEXUSREPO\\${lastpath}
+                    --username \$NEXUS_USERNAME --password \$NEXUS_PASSWORD"""
                 sh "helm nexus-push helm-private-repo ./${f} -u \$NEXUS_USERNAME -p \$NEXUS_PASSWORD"
             }
             }
