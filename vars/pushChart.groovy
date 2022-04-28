79afcaf21f97cca99f7ba0b14beb5a5979e1fb51
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-28 10:26:40 +0700

    testing push helm chart mechanism

--- vars/pushChart.groovy
+++ vars/pushChart.groovy
@@ -22,10 +22,15 @@ def call(Map args) {
 def pushChart(Map args) {
     echo "Push Chart"
     try {
-        sh "aws s3 cp --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-latest.tgz ./"
-        sh "aws s3 cp --endpoint-url ${args.spaces_url} ./${args.service_name}-latest.tgz s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-${dstVersion}.tgz"
+        sh """ 
+        aws s3 cp --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-latest.tgz ./${args.service_name}-${dstVersion}.tgz"
+        aws s3 cp --endpoint-url ${args.spaces_url} ./${args.service_name}-${dstVersion}.tgz s3://helm-charts/${args.application_name}/${args.environment}/
+        rm ${args.service_name}-${dstVersion}.tgz
+        """
     } catch (e) {
+        echo "first time push for helm chart service"
+    } finally {
         sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-latest.tgz"
-    }   
+    }  
     // sh "aws s3 cp s3://helm-charts/${args.namespace}/beta/${args.service_name}-*.tgz s3://helm-charts/${args.namespace}/beta/${args.service_name}-${args.dstVersion}.tgz"
 }
