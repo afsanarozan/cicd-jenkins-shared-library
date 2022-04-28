b496ef6a5442a13db694009ca3b0b1b6e75b9da5
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-28 10:13:02 +0700

    testing push helm chart mechanism

--- vars/pushChart.groovy
+++ vars/pushChart.groovy
@@ -10,26 +10,22 @@ def call(Map args) {
     } else {
         envar.environment = 'alpha'
     }
+    
     dir('Charts') {
-        sh "ls -lah"
-        container('s3cmd'){
-            // sh "s3cmd --configure --access_key=YFRP3PS4LIJEOZVRUMMK --secret_key=0s4FQ470cF9AGDg7old5fLyvvhbhnqO99ooruvQdVOs --region=sgp1 --host=sgp1.digitaloceanspaces.com --host-bucket=%(bucket)s.sgp1.digitaloceanspaces.com -s --no-encrypt --dump-config 2>&1 | tee /root/.s3cfg"
-            // sh "cat /root/.s3cfg"
-            // sh "s3cmd get s3://labs-storage/helm-charts/api-gateway/alpha/user-service-0.1.0.tgz" 
-            sh "s3cmd ls"
+        sh "ls"
+            container('aws-cli'){
+            pushChart(service_name: config.service_name, spaces_url: config.spaces_url, application_name: config.application_name, environment: envar.environment, dstVersion: "${BUILD_NUMBER}")
         }
     }
-    
-    // dir('Charts') {
-    //     sh "ls"
-    //         container('aws-cli'){
-    //         pushChart(service_name: config.service_name, spaces_url: config.spaces_url, application_name: config.application_name, environment: envar.environment, dstVersion: "${BUILD_NUMBER}")
-    //     }
-    // }
 }
 
 def pushChart(Map args) {
     echo "Push Chart"
-    sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/"
+    try {
+        sh "aws s3 cp --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-latest.tgz ./"
+        sh "aws s3 cp --endpoint-url ${args.spaces_url} ./${args.service_name}-latest.tgz s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-${dstVersion}.tgz"
+    } catch {
+        sh "aws s3 cp ${args.service_name}-*.tgz --endpoint-url ${args.spaces_url} s3://helm-charts/${args.application_name}/${args.environment}/${args.service_name}-latest.tgz"
+    }   
     // sh "aws s3 cp s3://helm-charts/${args.namespace}/beta/${args.service_name}-*.tgz s3://helm-charts/${args.namespace}/beta/${args.service_name}-${args.dstVersion}.tgz"
 }
