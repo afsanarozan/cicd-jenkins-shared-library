fd76fcae0728807a827aee0b0dce8e58d3bc2b95
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-04-28 15:06:57 +0700

    rollback

--- vars/telegramNotification.groovy
+++ vars/telegramNotification.groovy
@@ -9,7 +9,6 @@ def call(String buildStatus) {
 
     echo "Job Success"
     echo "${buildStatus}"
-    echo "${unitTest_score}"
     container('curl'){  
         if (buildStatus == 'STARTED') {
             notificationsStarted(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
