bcecee73622b27b779e83deba26c6433649cc503
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-09 11:36:50 +0700

    add score message

--- vars/telegramNotification.groovy
+++ vars/telegramNotification.groovy
@@ -17,7 +17,7 @@ def call(String buildStatus, score) {
         } else if (buildStatus == 'SUCCESS'){
             echo "${buildStatus}"
             echo "${score}"
-            notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
+            notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:score)
         }   else {
             notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
         }
@@ -33,7 +33,7 @@ def notificationsStarted (Map args) {
     sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
 }
 def notifications(Map args) {
-    def message = "CICD Pipeline ${args.JOB_NAME} ${args.buildStatus} with build ${args.BUILD_NUMBER} \n\n More info at: ${args.BUILD_URL} \n\n Unit Test: passed \n\n Total Time : ${currentBuild.durationString}"
+    def message = "CICD Pipeline ${args.JOB_NAME} ${args.buildStatus} with build ${args.BUILD_NUMBER} \n\n More info at: ${args.BUILD_URL} \n\n Unit Test: ${args.score} \n\n Total Time : ${currentBuild.durationString}"
     
     sh "curl -s -X POST ${args.telegram_url} -d chat_id=${args.telegram_chatid} -d text='${message}'"
     // parallel(
