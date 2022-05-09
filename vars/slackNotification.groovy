0b6791b29cf645574de73e0d9ab520275b545d29
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-09 15:59:06 +0700

    slack notification

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -23,23 +23,21 @@ def call(String buildStatus = 'STARTED') {
         def summary = "${subject} (${env.BUILD_URL})"
         def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+      } 
+
+      // Override default values based on build status
+      if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+      } else if (buildStatus == 'SUCCESS') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+      } else {
+        color = 'RED'
+        colorCode = '#FF0000'
       }
 
-      
-    } 
-
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESS') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  } else {
-    color = 'RED'
-    colorCode = '#FF0000'
-  }
-
-  // Send notifications
-  slackSend (color: colorCode, message: summary)
+      // Send notifications
+      slackSend (color: colorCode, message: summary)
+    }
 }
