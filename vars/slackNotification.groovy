782887aa59e535a30fb67c04299357df213f87ad
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-09 15:33:24 +0700

    edit slacknotification

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -13,8 +13,7 @@ def call(String buildStatus = 'STARTED') {
         def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
       } catch (e){
         def unitTestGetValue = 0.0%
-      } 
-      
+      } finally {
         // Default values
         def colorName = 'RED'
         def colorCode = '#FF0000'
@@ -22,7 +21,7 @@ def call(String buildStatus = 'STARTED') {
         def summary = "${subject} (${env.BUILD_URL})"
         def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-      
+      }
     } 
 
   // Override default values based on build status
