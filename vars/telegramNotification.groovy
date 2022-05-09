210102093ead67bc403ce6d6bd936a7064774954
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-09 13:07:12 +0700

    setup go tol to get coverage out

--- vars/telegramNotification.groovy
+++ vars/telegramNotification.groovy
@@ -4,10 +4,6 @@ def call(String buildStatus, String score) {
     sh "ls -la"
 
     def config = pipelineCfg()
-    def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
-    def unitTest_score   = "Your score is ${unitTestGetValue}"
-    echo "${unitTest_score}" 
-    sh "cat coverage.out"
 
     def telegram_chatid = -784775712
     def telegram_url    = "https://api.telegram.org/bot5117336515:AAFGksphWynQnpMlsF9dbqruHgFGRiM9-pw/sendMessage"
@@ -19,6 +15,10 @@ def call(String buildStatus, String score) {
             notificationsStarted(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
         } else if (buildStatus == 'SUCCESS'){
             echo "${buildStatus}"
+            def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
+            def unitTest_score   = "Your score is ${unitTestGetValue}"
+            echo "${unitTest_score}" 
+            sh "cat coverage.out"
             notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:score)
         }   else {
             notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL)
