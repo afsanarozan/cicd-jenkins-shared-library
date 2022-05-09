8dfbf8b23df8c94edd19e378a76afc73fd628eac
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-09 16:25:41 +0700

    slack notification

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -20,8 +20,8 @@ def call(String buildStatus = 'STARTED') {
         // Default values
         def colorName = 'RED'
         def colorCode = '#FF0000'
-        def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-        def summary = "${subject} (${env.BUILD_URL}) unit-testing: ${unitTestGetValue}"
+        def subject = "${buildStatus}: Job '${env.JOB_NAME} [${unitTestGetValue}]'"
+        def summary = "${subject} (${env.BUILD_URL})"
         def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
