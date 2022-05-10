b64ad334426249ad0c79f792d7ad2742ba4eec95
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-10 08:20:39 +0700

    test slack notification

--- vars/slackNotification.groovy
+++ vars/slackNotification.groovy
@@ -18,7 +18,7 @@ def call(String buildStatus = 'STARTED') {
         echo "${unitTestGetValue}"
         def score = "${unitTestGetValue}"
         echo "your score is ${score}"
-      } 
+      } finally {
         // Default values
         def colorName = 'RED'
         def colorCode = '#FF0000'
@@ -27,20 +27,21 @@ def call(String buildStatus = 'STARTED') {
         def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
           <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
-      // Override default values based on build status
-      if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-      } else if (buildStatus == 'SUCCESS') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
-      } else {
-        color = 'RED'
-        colorCode = '#FF0000'
-      }
+        // Override default values based on build status
+        if (buildStatus == 'STARTED') {
+          color = 'YELLOW'
+          colorCode = '#FFFF00'
+        } else if (buildStatus == 'SUCCESS') {
+          color = 'GREEN'
+          colorCode = '#00FF00'
+        } else {
+          color = 'RED'
+          colorCode = '#FF0000'
+        }
 
-      // Send notifications
-      echo "${score}"
-      slackSend (color: colorCode, message: summary)
+        // Send notifications
+        echo "${score}"
+        slackSend (color: colorCode, message: summary)
+      }
     }
 }
