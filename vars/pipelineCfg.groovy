8d0401669a0e71acac613cb2cebb07d77e5b64e0
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-12 13:47:03 +0700

    edit stage name

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -1,6 +1,6 @@
 def call() {
   sh "ls -lrth ${WORKSPACE}/"
-  sh "whoami"
+  echo "${env.STAGE_NAME}"
   sh "cat ${WORKSPACE}/pipeline.yaml"
   Map pipelineCfg = readYaml(file: "${WORKSPACE}/pipeline.yaml")
   return pipelineCfg
