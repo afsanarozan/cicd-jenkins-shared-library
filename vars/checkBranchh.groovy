6f9c7dc2b10c0592a7ea6ac521f759d6d2a61f7b
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-12 10:47:22 +0700

    add check branchh just for testing stage eroor notification

--- vars/checkBranchh.groovy
+++ vars/checkBranchh.groovy
@@ -2,7 +2,7 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-    if (envar.branch == '*/development'){
+    if (envar.branch == '*/developmt'){
         echo "passed"
     } else {
         echo "job success"
