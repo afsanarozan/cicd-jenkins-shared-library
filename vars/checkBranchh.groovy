cec2a1a9d7465fd8f61eaf29dff2136c9da33639
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-12 13:46:30 +0700

    testing

--- vars/checkBranchh.groovy
+++ vars/checkBranchh.groovy
@@ -2,7 +2,7 @@ def call() {
   def config = pipelineCfg()
   def envar = checkoutCode()
   print("ini :" + envar)
-    if (envar.branch == '*/developmt'){
+    if (envar.branch == '*/development'){
         echo "passed"
     } else {
         echo "job success"
