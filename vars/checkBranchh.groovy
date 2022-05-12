47a5bc813389aa3b6a9db0f897adb16cbc4d86c4
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-12 14:22:49 +0700

    testing variable STAGE NAME

--- vars/checkBranchh.groovy
+++ vars/checkBranchh.groovy
@@ -1,14 +1,20 @@
 def call() {
-  def config = pipelineCfg()
   def envar = checkoutCode()
-  print("ini :" + envar)
+  def env = [:]
+
+  try {
+    print("ini :" + envar)
     if (envar.branch == '*/development'){
         echo "passed"
     } else {
         echo "job success"
         error "This pipeline stops here!"
-        return env
     }
+  } catch {
+      echo "${env.STAGE_NAME}"
+      return env
+  }
+ 
         
 }
 
