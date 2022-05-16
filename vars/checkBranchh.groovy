d22fd9a0cd21072313f34b8265be7ee9a5d2692d
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-16 08:00:22 +0700

    testing

--- vars/checkBranchh.groovy
+++ vars/checkBranchh.groovy
@@ -14,7 +14,7 @@ def call() {
     // }
   } catch (e) {
       echo "This is Stage : ${env.STAGE_NAME}"
-      def res.STAGE_ERROR = env.STAGE_NAME
+      def res.STAGE_ERROR = "${env.STAGE_NAME}"
       echo "${res.STAGE_ERROR}"
       error "This pipeline stops here!"
       
