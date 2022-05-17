84af472cd726e5503cbb3c7a2c66ba7456ef438c
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 15:06:28 +0700

    testing

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -1,9 +1,8 @@
 def call () {
-    def stg = [:]
     def stage_name = "${env.STAGE_NAME}"
-    stg.error = "${stage_name}"
-    echo "error on stage ${stg.error}"
+    echo "error on stage ${env.STAGE_NAME}"
+    echo "error on stage ${stage_name}"
 
-    return stg 
+    // return stg 
 
 }
