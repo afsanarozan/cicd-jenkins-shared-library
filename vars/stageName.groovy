1d4b3a951e154f5e116acf5b482bf7eeea4eee59
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 13:01:10 +0700

    testing stageName

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -1,6 +1,6 @@
-def call (Map args) {
+def stageName (Map args) {
     def stg = [:]
-    
+
     echo "${args.stage_name}"
     stg.error = "${args.stage_name}"
     echo "error on stage ${stg.error}"
