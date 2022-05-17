09fb4782012763b2ec1b2dc66da77b7751a53f48
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 17:08:51 +0700

    testing

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -2,6 +2,8 @@ def call (Map args) {
 
     def stage_name = "${args.nameStage}"
     echo "fix iki seko kene : ${stage_name}"
+
+    return stage_name
     // def stage_name = "${test}"
     // echo "error on stage ${stage_name}" 
 
