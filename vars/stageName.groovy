87f37788774da95607dc71480e0b67a7aa366297
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 16:31:52 +0700

    testing

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -2,8 +2,5 @@ def call (String test = "init") {
     def stage_name = "${test}"
     echo "error on stage ${stage_name}" 
 
-    Map stage = stage_name
-    echo "fix ${stage}"
-
     return stage
 }
