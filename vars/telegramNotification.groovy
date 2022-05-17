cebdc423793d36c57d5dcd961ae86e10a27ff69c
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-18 06:36:15 +0700

    edit telegram notification

--- vars/telegramNotification.groovy
+++ vars/telegramNotification.groovy
@@ -2,8 +2,8 @@ def call(String buildStatus = 'STARTED') {
     // build status of null means successful
     buildStatus = buildStatus ?: 'SUCCESS'
     def config = pipelineCfg() 
-    sh "touch stageName.yaml"
-    def stg = readYaml(file: "${WORKSPACE}/stageName.yaml")
+    // sh "touch stageName.yaml"
+    // def stg = readYaml(file: "${WORKSPACE}/stageName.yaml")
     // def stg = stageName()
     // echo "${stg.stage_name}"
 
@@ -19,13 +19,13 @@ def call(String buildStatus = 'STARTED') {
             def root = tool type: 'go', name: 'Go'
             withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
                 def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
-                notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:unitTestGetValue, STAGE_NAME: stg.stage_name)
+                notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:unitTestGetValue, STAGE_NAME: config.stage_name)
             }
         }   else {
             def root = tool type: 'go', name: 'Go'
             withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin"]) {
                 def unitTestGetValue = sh(returnStdout: true, script: 'go tool cover -func=coverage.out | grep total | sed "s/[[:blank:]]*$//;s/.*[[:blank:]]//"')
-                notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:unitTestGetValue, STAGE_NAME: stg.stage_name)
+                notifications(buildStatus: buildStatus, telegram_chatid: telegram_chatid, telegram_url:telegram_url, JOB_NAME:env.JOB_NAME, BUILD_NUMBER:env.BUILD_NUMBER, BUILD_URL:env.BUILD_URL, score:unitTestGetValue, STAGE_NAME: config.stage_name)
             }
         }
     }
