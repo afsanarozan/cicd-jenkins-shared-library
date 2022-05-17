4a19c8f88d3d50484ed6f0d8acbc49511bc81eb6
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 15:02:50 +0700

    testing

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -1,5 +1,6 @@
-def call (String stage_name = 'Checkout Code') {
+def call () {
     def stg = [:]
+    def stage_name = "${env.STAGE_NAME}"
     stg.error = "${stage_name}"
     echo "error on stage ${stg.error}"
 
