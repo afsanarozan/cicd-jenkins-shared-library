0adb03fd62c9f12d2fc209ff9e76b9746682a27b
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-18 06:30:20 +0700

    testing using yq

--- vars/stageName.groovy
+++ vars/stageName.groovy
@@ -3,6 +3,13 @@ def call (Map args) {
     def stage_name = "${args.nameStage}"
     echo "fix iki seko kene : ${stage_name}"
     sh "echo stage_name : ${args.nameStage} > stageName.yaml"
+    container('yq') {
+        sh "ls"
+        sh """ 
+        yq -i '.stage_name = "${args.nameStage}"' pipeline.yaml
+        cat pipeline.yaml
+        """
+    }
 
     // def stage_name = "${test}"
     // echo "error on stage ${stage_name}" 
