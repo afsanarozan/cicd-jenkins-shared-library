8ec61e335a7fbc78363782e2d952e540e925cd74
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-17 09:50:34 +0700

    testing

--- vars/checkBranchh.groovy
+++ vars/checkBranchh.groovy
@@ -1,6 +1,6 @@
 def call() {
   def envar = checkoutCode()
-
+  def test = [:]
   try {
     error "This pipeline stops here!"
     // print("ini :" + envar)
@@ -13,9 +13,9 @@ def call() {
     // }
   } catch (e) {
       echo "This is Stage : ${env.STAGE_NAME}"
-      def test.testing = "${env.STAGE_NAME}"
+      test.testing = "${env.STAGE_NAME}"
       echo "${test.testing}"
-      return test.testing
+      return test
   } finally {
       error "This pipeline stops here!"
   }
