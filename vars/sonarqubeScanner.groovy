e79366f30e0801f8476ed58e69adc2bd55b01b16
Afsana Rozan Naufal <afsanarozanaufal@gmail.com>
2022-05-24 09:18:32 +0700

    edit variable

--- vars/sonarqubeScanner.groovy
+++ vars/sonarqubeScanner.groovy
@@ -4,14 +4,14 @@ def config = pipelineCfg()
         container('sonarscanner'){
         echo "Running Code Review With SonarQube"
                 withSonarQubeEnv(credentialsId: "sonarqube-token", installationName: 'sonarqube') {
-                sonarScanGo(name_space: config.name_space, service_name: config.service_name, sonarSrc: config.sonarSrc)
+                sonarScanGo(application_name: config.application_name, service_name: config.service_name, sonarSrc: config.sonarSrc)
                 }       
         }
 }
 
 def sonarScanGo(Map args){
         sh " sonar-scanner -X \
-        -Dsonar.projectKey=${args.name_space}-${args.service_name} \
+        -Dsonar.projectKey=${args.application_name}-${args.service_name} \
         -Dsonar.sources=${args.sonarSrc} \
         -Dsonar.qualitygate.wait=true \
         -Dsonar.language=go \
